9f5b83ede934fedbf88a6070c64d526f3b9c9961
John Beredimas <mperedim@ceid.upatras.gr>
2009-02-14 18:20:32 +0200

    Resolving ticket 78: checkout dir should not be under /tmp

--- settings.py
+++ settings.py
@@ -164,7 +164,7 @@ INSTALLED_APPS = [
 # The directory where the vcs app will checkout stuff and play around.
 # Warning: On production systems this should be a place outside of the source
 # and with enough disk space. Eg. /var/lib/transifex.
-SCRATCH_DIR = os.path.join('/tmp', 'scratchdir')
+SCRATCH_DIR = os.path.join('/var/lib/transifex', 'scratchdir')
 
 # The VCS choices for the system. Enable or disable any VCS types from here.
 # This list also feeds the Unit model with the available options.
