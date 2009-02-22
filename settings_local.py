f77b59965ca4ce90b27ce72c46ea05be6291cf49
Dimitris Glezos <dimitris@glezos.com>
2009-02-22 19:26:18 +0200

    trivial: settings_local minor fix, ignore by VCS

--- settings_local.py
+++ settings_local.py
@@ -7,6 +7,8 @@ by importing them at the end of the file. This means they also
 appear as if they were defined there.
 """
 
+from settings import *
+
 # Email sending
 #EMAIL_HOST = ''
 #EMAIL_HOST_USER = ''
@@ -15,8 +17,6 @@ appear as if they were defined there.
 #EMAIL_PORT = 587
 
 #SCRATCH_DIR = os.path.join('/tmp', 'scratchdir')
-
-from settings import *
 
 # To enable the use of the notification app on your system:
 #ENABLE_NOTICES = True
