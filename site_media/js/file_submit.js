78d5a05440aee75f6328872beefb80de1aa71624
Dimitris Glezos <dimitris@glezos.com>
2009-02-26 14:39:59 +0200

    trivial: Only show one submit form in a page.
    Close all open submit forms before opening a new one.

--- site_media/js/file_submit.js
+++ site_media/js/file_submit.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $('a.submit_button').click(function() {
+    if ( $(this).next("form.submit_form").is(':hidden') ) { 
+      $("form.submit_form").hide("slide");
+    }
     $(this).next("form.submit_form").toggle("medium");
-  });
+   })
 });
