03746fcb75f8b85ff10d7c3cf6c4569db1efd31e
Diego Búrigo Zacarão <diegobz@fedoraproject.org>
2009-02-26 15:45:54 -0300

    Minor: Fixing submit jquery actions and some typos as well

--- site_media/js/file_submit.js
+++ site_media/js/file_submit.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('a.submit_button').click(function() {
+  $('span.submit_button').click(function() {
     if ( $(this).next("form.submit_form").is(':hidden') ) { 
       $("form.submit_form").hide("slide");
     }
