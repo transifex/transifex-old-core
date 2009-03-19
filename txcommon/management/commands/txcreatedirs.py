30e8d6e01e5a4dc1bf104344218235088ebc2f2f
Christos Trochalakis <yatiohi@ideopolis.gr>
2009-03-18 23:03:08 -0400

    Introduce txcreatedirs command
        `python manage.py txcreatedirs` creates all the necessary
        directories specified in the settings file.
    
        Creates:
        * SCRATCH_DIR
        * MSGMERGE_DIR
        * REPO_PATHS

---
+++ txcommon/management/commands/txcreatedirs.py
@@ -0,0 +1,28 @@
+import os
+from django.core.management.base import NoArgsCommand
+
+def transifex_paths():
+    from django.conf import settings as s
+    # Scratch dir
+    yield s.SCRATCH_DIR
+    # Msgmerge dir
+    yield s.MSGMERGE_DIR
+    # Repo paths
+    for vcs in s.VCS_CHOICES.keys():
+        yield s.REPO_PATHS[vcs]
+
+
+class Command(NoArgsCommand):
+    help = 'Create scratchdir vcs directories'
+
+    requires_model_validation = False
+    can_import_settings = True
+    
+    def handle_noargs(self, **options):
+        for path in transifex_paths():
+            try:
+                os.makedirs(path)
+                print "Creating %s" % path
+            except OSError, e:
+                print "Error creating %s: %s" % (path, e.strerror)
+
