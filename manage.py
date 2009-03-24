605cf971cbb611973fcb8a5c94499bea66d35fbe
Giorgos Keramidas <keramida@ceid.upatras.gr>
2009-03-24 02:45:37 +0200

    Cleanup the toplevel manage.py file a bit
    - Avoid repeating 'settings.py' as a filename.
    - Display the directory where we expected to find `settings.py', instead
      of "the directory containing X".  Showing the actual pathname may be
      more useful.
    - Trim the excessively long error message to something a bit more terse.
      The users of `manage.py' probably don't really care about import
      exceptions; if they did we should display str(inst) of the exception
      instead of a guess.

--- manage.py
+++ manage.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+
 from django.core.management import execute_manager
+
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
+    import os
     import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+
+    dname = os.path.dirname(__file__)
+    sfile = 'settings.py'
+    err = sys.stderr.write
+
+    err("ERROR: Cannot locate `%s/%s'\n" % (dname, sfile))
+    err("Please re-run `django-admin.py' on the appropriate `%s' file.\n" % sfile)
     sys.exit(1)
 
 if __name__ == "__main__":
