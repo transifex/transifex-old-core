963a008654ca6b9dc2ba0016d4f2c168ba78a84a
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-05-03 22:21:36 -0300

    Added CharField validation for ValidRegexField as well
      - Now Tx will only validate the re after pass trough the
        CharField validation. (required=true works now)

--- transifex/txcommon/forms.py
+++ transifex/txcommon/forms.py
@@ -4,11 +4,13 @@ from django.forms import CharField, ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 class ValidRegexField(CharField):
-    def __init__(self, **kwargs):
-        super(ValidRegexField, self).__init__(**kwargs)
+    def __init__(self, max_length=None, min_length=None, error_message=None, *args, **kwargs):
+        super(ValidRegexField, self).__init__(max_length, min_length, *args, **kwargs)
 
     def clean(self, value):
+        value = super(ValidRegexField, self).clean(value)
         try:
             return re.compile(value).pattern
         except re.error, e:
             raise ValidationError(_('Enter a valid regular expression.'))
+
