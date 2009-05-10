b16f3a87bf84996c00ff9505782488ed02837104
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-05-09 22:48:11 -0300

    Added tooltip for submission note

--- transifex/site_media/js/base.js
+++ transifex/site_media/js/base.js
@@ -1,3 +1,33 @@
+
+// General Tooltip function
+function tooltip(targetnode, message){
+
+    $(targetnode).qtip({
+      content: message,
+      position: {
+         corner: {
+            target: 'topRight',
+            tooltip: 'bottomLeft'
+         }
+      },
+      style: {
+         name: 'cream',
+         color: '#685D40',
+         padding: '7px 13px',
+         width: {
+            max: 350,
+            min: 0
+         },
+         border: {
+            width: 3,
+            radius: 3
+         },
+         tip: true
+      }
+    })
+}
+
+
 $(document).ready(function(){
     
       // Enable autosubmit form after a change on the language drop box switcher
