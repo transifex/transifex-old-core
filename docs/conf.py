4812d8319861df734208eae6b27ad1269c7f5a32
Dimitris Glezos <glezos@indifex.com>
2009-08-12 05:35:35 +0300

    docs: Minor theme redesign (mostly colors).

--- docs/conf.py
+++ docs/conf.py
@@ -86,14 +86,14 @@ html_style = 'default.css'
 # "<project> v<release> documentation".
 #html_title = None
 
-# The name of an image file (within the static path) to place at the top of
-# the sidebar.
-#html_logo = None
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The name of an image file (within the static path) to place at the top of
+# the sidebar.
+html_logo = '_static/transifex-logo.png'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
