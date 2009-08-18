4ccdf711d214cb34324cb1aa7a7d37635467bc0e
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-08-18 13:32:07 -0300

    bugfix: Fixed access for locking files (kind of related to #300)
     - People that hava access to submit files for a project
       must be able to lock files too.

--- transifex/projects/views.py
+++ transifex/projects/views.py
@@ -796,6 +796,7 @@ def component_submit_file(request, project_slug, component_slug,
 
 pr_component_lock_file = (
     ('granular', 'project_perm.maintain'),
+    ('granular', 'project_perm.submit_file'),
     ('general',  'translations.add_pofilelock'),
     ('general',  'translations.delete_pofilelock'),
 )
