ccb1bf7dcb320c66ff8287ede06e394e312edfb1
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-08-27 15:28:05 -0300

    Added txcreatenoticetypes management command
     - As the south migration app cuts and handles the post_syncdb
       signal, it's necessary to create a management command for
       creating the notice types with more flexibility, as we can't
       ensure it will be always called after the post_syncdb signal.

---
+++ transifex/txcommon/management/commands/txcreatenoticetypes.py
@@ -0,0 +1,21 @@
+import os
+from django.core.management.base import NoArgsCommand
+from notification import models as notification
+from txcommon.notifications import NOTICE_TYPES
+
+def create_notice_types():
+    for n in NOTICE_TYPES:
+            notification.create_notice_type(n["label"], n["display"], 
+                                            n["description"], n["default"])
+
+class Command(NoArgsCommand):
+    help = 'Create or Update the notice types used in the ActionLog and ' \
+           'Notification apps'
+
+    requires_model_validation = False
+    can_import_settings = False
+
+    def handle_noargs(self, **options):
+        print "Creating or updating notice types"
+        create_notice_types()
+        print "Default set of notice types initialized successfully."
