978583953005782c4fd328cc786c9a0c20df0711
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-08-27 13:08:10 -0300

    bugfix: Notice types now are mandatory. It shouldn't be skipped.

--- transifex/txcommon/management/__init__.py
+++ transifex/txcommon/management/__init__.py
@@ -1,18 +1,14 @@
 from django.conf import settings
 from django.db.models import signals
+from notification import models as notification
 from txcommon import models as txcommon_app
 from txcommon.notifications import NOTICE_TYPES
 
-try:
-    from notification import models as notification
+def create_notice_types(app, created_models, verbosity, **kwargs):
+    for n in NOTICE_TYPES:
+            notification.create_notice_type(n["label"],
+                                            n["display"], 
+                                            n["description"], 
+                                            n["default"])
 
-    def create_notice_types(app, created_models, verbosity, **kwargs):
-        for n in NOTICE_TYPES:
-                notification.create_notice_type(n["label"],
-                                                n["display"], 
-                                                n["description"], 
-                                                n["default"])
-
-    signals.post_syncdb.connect(create_notice_types, sender=txcommon_app)
-except ImportError:
-    print "Skipping creation of NoticeTypes as notification app not found"
+signals.post_syncdb.connect(create_notice_types, sender=txcommon_app)
