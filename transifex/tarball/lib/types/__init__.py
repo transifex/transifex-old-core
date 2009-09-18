f5b1af1309b53b03fe8f6542e1601409fe8ef67c
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-09-18 12:22:05 -0300

    trivial: Less noise when sending a file for a tarball component

--- transifex/tarball/lib/types/__init__.py
+++ transifex/tarball/lib/types/__init__.py
@@ -79,8 +79,6 @@ class BrowserMixin:
                 for chunk in contents:
                     if encode:
                         chunk = chunk.encode(encode)
-                    logger.debug("Saving %s bytes in file %s" %
-                                 (len(contents), fullpath))
                     fp.write(chunk)
             else:
                 raise ValueError("Given content to save to %s that I don't "
