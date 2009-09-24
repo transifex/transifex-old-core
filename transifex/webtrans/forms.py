ec500d8ee5b10ddb413a49e67f32b699fa67df88
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-09-24 14:46:46 -0300

    lotte: Moved field/widget classes to its own file

--- transifex/webtrans/forms.py
+++ transifex/webtrans/forms.py
@@ -1,20 +1,10 @@
-import re
 import polib
 from django import forms
-from django.utils.safestring import mark_safe
 from django.template.loader import render_to_string
 
+from webtrans.fields import MessageField, PluralMessageField
 
-def calculate_rows(entry):
-    text = getattr(entry, 'msgid', entry)
-    if isinstance(text, str):
-        text = text.decode(getattr(entry, 'encoding', 'UTF-8'))
-    replacement = polib.escape(text).replace(r'\n','<br />\n')
-    lines = mark_safe(replacement).split(u'\n')
-    return sum(len(line)/40 for k, line in enumerate(lines)) + 1
-
-
-def guess_entry_status(entry):
+def _guess_entry_status(entry):
     if entry.translated() and not entry.obsolete:
         return 'translated'
     elif 'fuzzy' in entry.flags:
@@ -22,59 +12,10 @@ def guess_entry_status(entry):
     elif not entry.translated() and not entry.obsolete:
         return 'untranslated'
 
-
 def _get_label(msgid_list):
     """Return the label for a plural field."""
     return render_to_string('webtrans/msgid_label.html',
                             { 'msgid_list': msgid_list})
-
-
-class PluralMessageWidget(forms.MultiWidget):
-    """A widget to render plural translatable strings."""
-    def __init__(self, messages, attrs=None):
-        widgets=[]
-        for i, entry in enumerate(messages):
-            widgets.append(forms.Textarea(attrs=attrs))
-        super(PluralMessageWidget, self).__init__(widgets, attrs)
-
-    def decompress(self, value):
-        if value:
-            return value.split('#|#')
-        return ['']
-
-
-class PluralMessageField(forms.MultiValueField):
-    """A field for plural translatable strings."""
-    def __init__(self, entry, initial, attrs={}, widget=None, *args, **kwargs):
-        attrs.update({'rows': calculate_rows(entry)})
-        fields = []
-        for i in initial:
-            fields.append(forms.CharField(label=entry.msgid_plural))
-
-        if not widget:
-            widget=PluralMessageWidget(initial, attrs=attrs)
-        super(PluralMessageField, self).__init__(fields, initial=initial, 
-            widget=widget, *args, **kwargs)
-
-    def compress(self, data_list):
-        if data_list:
-            return "#|#".join(unicode(data) for data in data_list)
-        return None
-
-
-class MessageField(forms.CharField):
-    """A field for translatable strings."""
-    def __init__(self, entry, attrs={}, widget=None, *args, **kwargs):
-        attrs.update({'rows': calculate_rows(entry)})
-        if not widget:
-            widget=MessageWidget(attrs=attrs)
-        super(MessageField, self).__init__(attrs, widget=widget, *args, **kwargs)
-
-
-class MessageWidget(forms.Textarea):
-    """A widget to render translatable strings."""
-    def __init__(self, attrs=None, *args, **kwargs):
-        super(MessageWidget, self).__init__(attrs, *args, **kwargs)
 
 
 class TranslationForm(forms.Form):
@@ -84,7 +25,7 @@ class TranslationForm(forms.Form):
         k=1;
         for entry in po_entries:
             if entry.obsolete == 0:
-                entry_status = guess_entry_status(entry)
+                entry_status = _guess_entry_status(entry)
                 fuzzy = False
                 if entry_status == 'fuzzy':
                     fuzzy = True
