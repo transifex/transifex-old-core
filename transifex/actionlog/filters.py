6258364914f4a66c9962700ec35e96eac5fe27c5
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-12-21 10:26:00 -0200

    Added DateRangeFilter on the timeline forms
     - It allows the actions to be filtered by anytime, today,
       week, month, year.

--- transifex/actionlog/filters.py
+++ transifex/actionlog/filters.py
@@ -2,6 +2,7 @@ import django_filters
 from actionlog.models import LogEntry
 
 class LogEntryFilter(django_filters.FilterSet):
+    action_time = django_filters.DateRangeFilter()
     class Meta:
         model = LogEntry
-        fields = ['user', 'action_type']
+        fields = ['user', 'action_type', 'action_time']
