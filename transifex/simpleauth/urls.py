458968e3f046f7d7a89935bc58f152b77543475c
Diego Búrigo Zacarão <diegobz@indifex.com>
2009-12-24 10:01:25 -0200

    Added a public profile to the simpleauth app

--- transifex/simpleauth/urls.py
+++ transifex/simpleauth/urls.py
@@ -6,10 +6,11 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 from django.contrib.auth import views as auth_views
 from django.utils.translation import ugettext as _
-from simpleauth.views import login, logout, account_settings
+from simpleauth.views import login, logout, account_settings, profile_public
 
 urlpatterns = patterns('',
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^$', account_settings, name='profile_overview'),
+    url(r'^profile/(?P<username>.+)/$', profile_public, name='profile_public'),
 )
