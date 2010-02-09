b1245acb6b0fcf6e72acd2b6e275a2d498735c04
Dimitris Glezos <glezos@indifex.com>
2010-02-09 11:17:16 +0200

    bugfix: Cleanup some migrations and fix latest regression.

---
+++ transifex/txcollections/__init__.py
@@ -0,0 +1,7 @@
+"""
+The txcollections application used to work, but today it isn't being used. 
+
+It is here to allow South to do its migrations peacefully, because it
+doesn't yet support the removal of applications. In future versions of
+Transifex and South, this application will be removed.
+"""
