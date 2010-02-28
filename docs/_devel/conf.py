ef8c364f9b13f652dd96a75dee77570e06feb56b
Dimitris Glezos <glezos@indifex.com>
2010-02-28 20:50:58 +0200

    Docs directory structure overhaul.
    Now our docs directory should only have the following dirs::
    
      plaintext: The source docs in plaintext form (human-readable).
      html     : An HTML-rendered forms of the docs
      _devel   : All necessary resources to build the docs.
    
    A couple of README files are available for your pleasure. =)
    * * *
    docs: Some more files moved around.

--- docs/_devel/conf.py
+++ docs/_devel/conf.py
@@ -16,7 +16,8 @@ import os
 from txcommon import version as txversion, version_info
 
 # If your extensions are in another directory, add it here.
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../transifex')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+'../../transifex')))
 
 
 # General configuration
