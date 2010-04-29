f70ce92c8755dacbd2683766b2b33ec1fc5fba8e
Diego Búrigo Zacarão <diegobz@indifex.com>
2010-04-29 17:12:38 -0300

    Added txcron addon
     - Code by Lauri Võsandi <lauri@indifex.com>

---
+++ transifex/addons/txcron/signals.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.dispatch import Signal
+
+# 00:00:00
+cron_nightly = Signal()
+
+# 12:00:00
+cron_daily = Signal()
+
+# 06:00:00 || 18:00:00
+cron_bidaily = Signal()
+
+# xx:00:00
+cron_hourly = Signal()
