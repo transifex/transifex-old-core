6768b5d32fd8662425d6099fed9cfc2c871dc8e7
Diego Búrigo Zacarão <diegobz@indifex.com>
2010-05-05 18:13:39 -0300

    Added unittest base lib.
     - It must be used as a base for unittests that needs some
       initial data already created. It provides a good set of
       pre-defined objects for testing purposes.

---
+++ transifex/txcommon/tests/base.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+import os
+from django.contrib.auth import models
+from django.contrib.contenttypes.models import ContentType
+from django.core import management
+from django.conf import settings
+from django.db import IntegrityError
+from django.db.models.loading import get_model
+from django.contrib.auth.models import User, Group
+from django.test import TestCase
+from django.test.client import Client
+from django_addons.autodiscover import autodiscover_notifications
+from txcommon.notifications import NOTICE_TYPES
+from vcs.tests import test_git
+
+# Load models
+Project = get_model('projects', 'Project')
+Component = get_model('projects', 'Component')
+Release = get_model('projects', 'Release')
+Team = get_model('teams', 'Team')
+Language = get_model('languages', 'Language')
+Permission = get_model('authority', 'Permission')
+POFile = get_model('translations', 'POFile')
+
+USER_ROLES = [
+    'anonymous',
+    'registered',
+    'maintainer',
+    'writer',
+    'team_coordinator',
+    'team_member']
+
+class BaseTestCase(TestCase):
+    """
+    Creates a sample set of object such as project, component, release, team,
+    users, permission, etc. and does a checkout calculating the po files
+    statistics.
+    """
+    # Use the vcs app git test repo
+    root_url = '%s/test_repo/git' % os.path.split(test_git.__file__)[0]
+
+    def setUp(self, skip_stats = False):
+        """
+        Set up project, component and vcsunit. Insert POFile objects.
+        """
+
+        # I think it doesn't work if you just remove it here from 
+        # MIDDLEWARE_CLASSES list, it should be disabled in settings already
+        self.assertFalse('external.csrf.middleware.CsrfMiddleware' in \
+            settings.MIDDLEWARE_CLASSES, msg = "Running tests with CSRF " \
+            "middleware enabled is not supported!")
+
+        # Run management commands
+        management.call_command('txcreatelanguages')
+        autodiscover_notifications()
+        management.call_command('txcreatenoticetypes')
+
+        # Add group 'registered'
+        registered, created = Group.objects.get_or_create(name = "registered")
+
+        # Set proper permission to the 'registered' group
+        registered.permissions.add(
+            models.Permission.objects.get_or_create(
+                codename='add_project', name='Can add project',
+                content_type=ContentType.objects.get_for_model(Project))[0])
+
+        self.user = {}
+        self.client = {}
+        self.client['anonymous'] = Client()
+
+        prefix = 'test_suite'
+        password = '123412341234'
+        for nick in USER_ROLES:
+            if nick != 'anonymous':
+                try:
+                    user = User.objects.create_user('%s_%s' % (prefix, nick),
+                        '%s_%s@localhost' % (prefix, nick), password)
+                except IntegrityError:
+                    user = User.objects.get(username='%s_%s' % (prefix, nick))
+                user.groups.add(registered)
+                self.user[nick] = user
+
+                client = Client()
+                self.assertTrue(client.login(
+                    username = '%s_%s' % (prefix, nick), password = password))
+                self.client[nick] = client
+
+        # Create a project, a component/vcsunit a release, and a pt_BR team
+        self.project, created = Project.objects.get_or_create(
+            slug="test_project", name="Test Project")
+        self.project.maintainers.add(self.user['maintainer'])
+
+        self.component, created = Component.objects.get_or_create(
+            slug='test_component', project=self.project, i18n_type='POT',
+            file_filter='po/.*')
+        self.component.set_unit(self.root_url, 'git', 'master')
+
+        self.release = Release.objects.get_or_create(slug="r1", name="r1",
+            project=self.project)[0]
+        self.release.components.add(self.component)
+
+        self.language = Language.objects.get(code='pt_BR')
+        self.team = Team.objects.get_or_create(language=self.language,
+            project=self.project, creator=self.user['maintainer'])[0]
+        self.team.coordinators.add(self.user['team_coordinator'])
+        self.team.members.add(self.user['team_member'])
+
+        # Add django-authority permission for writer
+        self.permission = Permission(codename='project_perm.submit_file', 
+            approved=True, user=self.user['writer'], 
+            content_object=self.project, creator=self.user['maintainer'])
+        self.permission.save()
+
+
+
+        if not skip_stats:
+            # Do a repo checkout and calculate the po file stats
+            self.component.prepare()
+            self.component.trans.set_stats()
+ 
+            # Fetch pofiles
+            self.pofiles = POFile.objects.filter(component = self.component)
+        else:
+            self.pofiles = None          
+        self.assertNotEqual(self.pofiles, None)
+
+    def tearDown(self):
+        self.project.delete()
+        for nick, user in self.user.iteritems():
+            user.delete()
+
+    def assertNoticeTypeExistence(self, noticetype_label):
+        """
+        Test if noticetype was added
+        """
+        found = False
+        for n in NOTICE_TYPES:
+             if n["label"] == noticetype_label:
+                 found = True
+        self.assertTrue(found, msg = "Notice type '%s' wasn't "
+            "added" % noticetype_label)
---
+++ transifex/txcommon/tests/utils.py
@@ -0,0 +1,132 @@
+from txcommon.tests.base import USER_ROLES
+from txcommon.log import logger
+
+def getitem(list, index, default=None):
+    """
+    Return a value from a 'list' for the given 'index' or the 'default' value.
+    """
+    try:
+        return list[index]
+    except IndexError:
+        return default
+
+def assert_status_code(unittest, response, expected_code, url, user_role):
+    unittest.assertEqual(response.status_code, expected_code,
+        "Status code for page '%s' was %s instead of %s for the '%s' "
+        "user role. \n%s" % (url, response.status_code, expected_code,
+        user_role, response))
+
+
+def check_page_status(unittest, user_role, url_roles):
+    """
+    Check if each URL set in 'url_roles' match its status_code with the 
+    response code status depending on the user role. Each URL can be added to 
+    the 'url_roles' dictionary using a predefined structure of data.
+
+    unittest - It's a instance of a class that inherits
+        commontests.tests.base.BaseTestCase.
+    user_role - One of the user roles allowed on
+        commontests.tests.base.BaseTestCase.
+    url_roles - A structured dictionary containing the url to be tested.
+
+    Structure of 'url_roles':
+        url_roles = {'<REQUEST_METHOD>:<URL>':{
+            '<USER_ROLE>':(<STATUS_CODE>, <DICT_OF_ARGS>,
+                <EXPECTED_CONTENTS>, <UNEXPECTED_CONTENTS>, <FOLLOW_REDIRECT>),
+            (...)
+            },
+        }
+
+        <REQUEST_METHOD> - Might be 'GET' or 'POST'.
+        <URL> - The url that must be tested.
+        <USER_ROLE> - One of the user roles allowed on 
+            commontests.tests.base.BaseTestCase. At least one user 
+            role is required.
+        <STATUS_CODE> - Code of status for the HTTP response.
+        <DICT_OF_ARGS> - Arguments that might be passed to the HTTP request. 
+            Not required.
+        <EXPECTED_CONTENTS> - A string that might be found using 
+            assertContains on the response contents. Not required.
+        <UNEXPECTED_CONTENTS> - A string that shouldn't be found. Not required.
+        <FOLLOW_REDIRECT> - If it's a redirect, follow it
+            
+    Know issues:
+        <EXPECTED_CONTENTS> usually does not work with the status code 302.
+    """
+    if not user_role in USER_ROLES:
+        unittest.fail("Unknown user role: '%s'" % user_role)
+
+    for url, date_dict in url_roles.items():
+        try:
+            expected_code = date_dict[user_role][0]
+            args = getitem(date_dict[user_role], 1, {})
+            expected_contents = getitem(date_dict[user_role], 2, None)
+            unexpected_contents = getitem(date_dict[user_role], 3, None)
+            follow_redirect = getitem(date_dict[user_role], 4, False)
+
+            client = unittest.client[user_role]
+
+            method, url = tuple(url.split(':'))
+            if method == 'GET':
+                response = client.get(url, args, follow=follow_redirect)
+            elif method == 'POST':
+                response = client.post(url, args, follow=follow_redirect)
+            else:
+                unittest.fail("Unknown method request: '%s'" % role)
+
+            assert_status_code(unittest, response, expected_code, url, 
+                user_role)
+
+            if expected_contents:
+                unittest.assertContains(response, expected_contents, 
+                    status_code=expected_code)
+            if unexpected_contents:
+                unittest.assertNotContains(response, unexpected_contents,
+                    status_code=expected_code)
+        except KeyError:
+            logger.info("User role '%s' not defined for the '%s' URL." % (
+                user_role, url))
+
+
+def convert_url_roles(url_with_roles_as_key):
+    """
+    Convert url roles from a dictionary using the roles as the key to a 
+    dictionary using the url as key.
+
+    'url_with_roles_as_key' is a dict in the following format:
+
+    url_with_roles_as_key = {
+        '(301, )':[
+            'GET:/projects/p/test_project/access/pm/add',
+        ],
+        '(200, {}, "Translation Teams Off")':[
+            'GET:/projects/p/test_project/teams/',
+        ],
+    }
+
+    Output:
+        {'GET:/projects/p/test_project/access/pm/add': {
+            'anonymous': (301,),
+            'maintainer': (301,),
+            'registered': (301,),
+            'team_coordinator': (301,),
+            'team_member': (301,),
+            'writer': (301,)},
+        'GET:/projects/p/test_project/teams/': {
+            'anonymous': (200, {}, 'Translation Teams Off'),
+            'maintainer': (200, {}, 'Translation Teams Off'),
+            'registered': (200, {}, 'Translation Teams Off'),
+            'team_coordinator': (200, {}, 'Translation Teams Off'),
+            'team_member': (200, {}, 'Translation Teams Off'),
+            'writer': (200, {}, 'Translation Teams Off')}}
+        }
+    """
+    url_keys = {}
+    for url_role, urls in url_with_roles_as_key.items():
+        user_roles_dict={}
+        url_role = eval(url_role)
+        for user_role in USER_ROLES:
+            user_roles_dict.update({user_role:url_role})
+        for url in urls:
+            url_keys.update({url:user_roles_dict})
+    return url_keys
