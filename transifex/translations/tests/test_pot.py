e824882fd60bd668eb4ad552f6cdc9dda2ec9fb1
Korkakakis Nikos <korki@indifex.com>
2010-05-07 14:44:01 +0300

    Fixed the translations unittests.

--- transifex/translations/tests/test_pot.py
+++ transifex/translations/tests/test_pot.py
@@ -14,64 +14,60 @@ class POTTestCaseBase(unittest.TestCase):
     def setUp(self):
 
         # Creating a project
-        self.p = Project.objects.create(slug="foo", name="Foo Project")
-        self.p.save()
+        self.project, created = Project.objects.get_or_create(
+            slug="test_project_lang", name="Test Project Langs")
+        self.component, created = Component.objects.get_or_create(
+            slug='test_component_lang', project=self.project, i18n_type='POT',
+            file_filter='po/.*', name='Test Component Langs')
 
         # Creating a component with an unit
-        self.c = Component(slug='bar', project=self.p, i18n_type='POT', 
-                           file_filter='po/.*')
-
+        self.reporoot = 'git://git.fedorahosted.org/git/desktop-effects.git'
         # Creating an unit
-        self.c.set_unit(branch='master', type='git',
-                  root='git://git.fedorahosted.org/git/docs/readme.git')
+        try:
+            self.component.unit = VcsUnit.objects.get(name=self.component.full_name)
+            self.component.save()
+        except:
+            self.component.set_unit(branch='master', type='git', root=self.reporoot)
 
-        self.c.save()
 
         # Initializing the component's unit
-        self.c.unit.init_browser()
+        self.component.unit._init_browser()
 
         # Unit checkout
-        self.c.unit.browser.init_repo()
-
-        self.tm = POTManager(self.c.get_files(),
-                             self.c.unit.browser.path, 
-                             self.c.source_lang)
+        self.component.prepare()
 
     def tearDown(self):
-        self.c.unit.browser.teardown_repo()
-        self.c.unit.delete()
-        self.c.delete()
-        self.p.delete()
+        self.component.unit.browser.teardown_repo()
+        self.component.delete()
+        self.project.delete()
 
 class POTTestCase(POTTestCaseBase):
 
     def test_calcule_stats(self):
         """Test that tm.browser.calcule_stats works properly."""
-
-        stats = self.tm.calcule_stats('pt_BR')
-        self.assertTrue(len(stats)>0)
-
-        try:
-            stats = self.tm.calcule_stats('--')
-        except POTStatsError:
-            pass
-        else:
-            fail("expected a POTStatsError")
+        self.component.trans.set_stats()
+        pofiles = self.component.trans.get_stats()
+        for pofile in pofiles:
+            self.assertEquals(pofile.fuzzy+pofile.trans+pofile.untrans,
+                pofile.total)
+        if self.component.trans.get_lang_stats('--'):
+            fail('lala')
 
     def test_get_langs(self):
         """Test that tm.browser.get_langs works properly."""
 
-        langs = self.tm.get_langs()
-        self.assertTrue(len(langs)>0)
+        langs = self.component.trans.tm.get_langs()
+        self.assertTrue(langs.next())
 
     def test_get_po_files(self):
         """Test that tm.browser.get_po_files works properly."""
 
-        pofiles = self.tm.get_po_files()
-        self.assertTrue(len(pofiles)>0)
+        pofiles = self.component.trans.tm.get_po_files()
+
+        self.assertTrue(pofiles.next())
 
     def test_get_langfile(self):
         """Test that tm.browser.get_langfile works properly."""
 
-        file = self.tm.get_langfile('pt_BR')
-        self.assertTrue(len(file)>0)
+        file = self.component.trans.tm.get_lang_files('pt_BR')
+        self.assertTrue(file.next())
