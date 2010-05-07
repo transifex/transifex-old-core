729b6e3f04a726b678909b14eb30d4801284a1fa
Korkakakis Nikos <korki@indifex.com>
2010-05-07 13:09:40 +0300

    Minor cleanup on the bzr repositories.

--- transifex/vcs/tests/test_bzr.py
+++ transifex/vcs/tests/test_bzr.py
@@ -12,6 +12,10 @@ class BzrTestCase(unittest.TestCase):
     def setUp(self):
         self.unit = VcsUnit.objects.create(type='bzr',
             root='%s/test_repo/bzr' % os.path.split(__file__)[0],)
+        self.unit._init_browser()
+        self.unit.browser.init_repo()
+        self.unit.browser.update()
+        
     def tearDown(self):
         self.unit.delete()
         # Until we use a local repo, let's not delete it after the first run:
@@ -21,16 +25,11 @@ class BzrTestCase(unittest.TestCase):
         """Test correct Bzr repo initialization."""
         from os import path
         from vcs.lib.types.bzr import REPO_PATH
-        self.unit._init_browser()
-        self.unit.browser.init_repo()
         local_unit_path = path.join(REPO_PATH, self.unit.name)
         self.assertTrue(path.isdir(local_unit_path))
 
     def test_get_file_contents(self):
         """Test that Bzr get_file_contents returns correct file size."""
         #FIXME: This is not the best way to test something like this!
-        self.unit._init_browser()
-        self.unit.browser.init_repo()
         self.assertEquals(len(self.unit.browser.get_file_contents(
-                          'po/test_repo.pot')),
-                          594)
+                          'po/test_repo.pot')), 594)
