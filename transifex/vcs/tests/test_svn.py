7c17f1bb3001468be82f74890d4e1611aec22df0
Korkakakis Nikos <korki@indifex.com>
2010-05-07 12:59:03 +0300

    Actual fix on the svn unittests.

--- transifex/vcs/tests/test_svn.py
+++ transifex/vcs/tests/test_svn.py
@@ -12,39 +12,37 @@ class SvnTestCase(unittest.TestCase):
     #TODO: Run the init stuff only when needed.
     def setUp(self):
 
-        root='%s/test_repo/svn/' % os.path.split(__file__)[0]
+        self.root = '%s/test_repo/svn/' % os.path.split(__file__)[0]
         #we need to remove any old checkout dir in order for it to exists within
         # our local paths
         try:
-            os.unlink('%scheckout' % root)
+            os.unlink('%scheckout' % self.root)
         except OSError:
             pass
         #now recreate this within to create all the necessary local paths
         stat = commands.getstatusoutput('svn co file://%s/svnrepo/ %scheckout'
-            % (root, root))
-        print stat
+            % (self.root, self.root))
         self.unit = VcsUnit.objects.create(
             name="Test-SVN",
-            root='%scheckout' % root,
+            root='%scheckout' % self.root,
             branch='trunk', type='svn')
+        self.unit._init_browser()
+        self.unit.browser.setup_repo()
+        self.unit.browser.update()  
+        
     def tearDown(self):
+        self.unit.browser.teardown_repo()
         self.unit.delete()
-        # Until we use a local repo, let's not delete it after the first run:
-        # self.unit.browser.teardown_repo()
-
+        
     def test_repo_init(self):
         """Test correct SVN repo initialization."""
         from os import path
         from vcs.lib.types.svn import REPO_PATH 
-        self.unit._init_browser()
-        self.unit.browser.setup_repo()
         local_unit_path = path.join(REPO_PATH, self.unit.name)
         self.assertTrue(path.isdir(local_unit_path))
 
     def test_get_file_contents(self):
         """Test that SVN get_file_contents returns correct file size."""
         #FIXME: This is not the best way to test something like this!
-        self.unit._init_browser()
-        self.unit.browser.init_repo()
-        self.assertEquals(len(self.unit.browser.get_file_contents('po/test_repo.pot')),
-                          594)
+        self.assertEquals(len(self.unit.browser.get_file_contents(
+            'po/test_repo.pot')), 594)
