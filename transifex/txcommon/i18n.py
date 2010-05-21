f875ac1fd09e02e5d59d76ff73ca7bde11efde45
Dimitris Glezos <glezos@indifex.com>
2010-05-21 13:00:08 +0300

    trivial: Better error logging if LINGUAS is not found.

--- transifex/txcommon/i18n.py
+++ transifex/txcommon/i18n.py
@@ -8,12 +8,14 @@ from django.utils.translation import ugettext as _
 def available_languages(localedir):
     """Return available languages in the LINGUAS file."""
     available_languages = []
+    linguas_file = os.path.join(localedir, 'LINGUAS')
     try:
-        linguas = codecs.open(os.path.join(localedir, 'LINGUAS'), 'r')
+        linguas = codecs.open(linguas_file, 'r')
         for lang in linguas.readlines():
             lang = lang.strip()
             if lang and not lang.startswith('#'):
                 available_languages.append((lang,lang))
     except IOError, e:
-        logging.error(_('The LINGUAS file could not be opened: %s') % e)
+        logging.error(_('The LINGUAS file (%s) could not be opened: %s') % 
+                        (linguas_file, e))
     return available_languages
