9ad47247d87022b1670ba752e342450b9097f892
Seraphim Mellos <mellos@indifex.com>
2010-07-05 12:25:57 +0300

    Fixed issue with cache invalidation

--- transifex/happix/handlers.py
+++ transifex/happix/handlers.py
@@ -4,9 +4,11 @@ from django.core.cache import cache
 from happix.models import HAPPIX_CACHE_KEYS
 
 # FIXME we can do more clever calculations instead of deleting the values
+# FIXME: if resource/project doesn't exist cache doesn't clear. maybe force
+# expire?
 def on_save_invalidate_cache(sender, instance, created, **kwargs):
     """Invalidate cache keys related to the SourceEntity updates"""
-    if instance:
+    if instance and instance.resource and instance.resource.project:
         cache.delete(HAPPIX_CACHE_KEYS["word_count"] % (instance.resource.project.slug,
             instance.resource.slug))
         if created:
@@ -17,7 +19,7 @@ def on_save_invalidate_cache(sender, instance, created, **kwargs):
 
 def on_delete_invalidate_cache(sender, instance, **kwargs):
     """Invalidate cache keys related to the SourceEntity updates"""
-    if instance:
+    if instance and instance.resource and instance.resource.project:
         cache.delete(HAPPIX_CACHE_KEYS["word_count"] % (instance.resource.project.slug,
             instance.resource.slug))
         cache.delete(HAPPIX_CACHE_KEYS["source_strings_count"]% (
