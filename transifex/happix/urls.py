b8971d1884b02995d897c56b24bad3fff0e8114a
Diego Búrigo Zacarão <diegobz@indifex.com>
2010-08-03 17:39:24 -0300

    Added /l/ prefix for URLs that require language code
     - Not it will look like ...resource/foo/l/pt_BR/ rather
       than ...resource/foo/pt_BR/

--- transifex/happix/urls.py
+++ transifex/happix/urls.py
@@ -12,8 +12,8 @@ urlpatterns = patterns('',
     url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/$', resource_detail, name='resource_detail'),
     url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/edit$', resource_edit, name='resource_edit'),
     url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/delete$', resource_delete, name='resource_delete'),
-    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/(?P<lang_code>[-\w]+)/delete_all/$', resource_translations_delete, name='resource_translations_delete'),
-    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/(?P<target_lang_code>[-\w]+)/actions/$', resource_actions, name='resource_actions'),
+    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<lang_code>[-\w]+)/delete_all/$', resource_translations_delete, name='resource_translations_delete'),
+    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<target_lang_code>[-\w]+)/actions/$', resource_actions, name='resource_actions'),
 
     # Project resources list
     url(r'^project/(?P<project_slug>[-\w]+)/resources/(?P<offset>\d+)$', project_resources, name='project_resources'),
@@ -21,8 +21,8 @@ urlpatterns = patterns('',
 
     # Overrides for N/A views
     url(r'^project/(?P<project_slug>[-\w]+)/(?P<lang_code>[-\w]+)/$', not_available, name='translate'),
-    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/(?P<lang_code>[-\w]+)/$', not_available, name='translate'),
-    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/(?P<lang_code>[-\w]+)/view/$', not_available, name='view_strings'),
-    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/(?P<lang_code>[-\w]+)/download/$', get_translation_file, name='download_translation'),
+    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<lang_code>[-\w]+)/$', not_available, name='translate'),
+    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<lang_code>[-\w]+)/view/$', not_available, name='view_strings'),
+    url(r'^project/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<lang_code>[-\w]+)/download/$', get_translation_file, name='download_translation'),
 
 )
