b8f5dc77b50aba436acb5e5cfab7f424dc4dce63
Dimitris Glezos <glezos@indifex.com>
2010-09-13 20:31:23 +0300

    Use PROJECT_PATH instead of __file__.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py as _build_py
 
 from transifex.txcommon import version
+from django.conf import settings
 
 readme_file = open(u'README')
 long_description = readme_file.read()
@@ -24,8 +25,7 @@ package_data = {
 def buildlanguages():
     import django.core.management.commands.compilemessages as c
     oldpath = os.getcwd()
-    os.chdir(os.path.join(os.path.dirname(__FILE__), 'transifex',
-        'locale'))
+    os.chdir(os.path.join(settings.PROJECT_PATH, 'locale'))
     c.compile_messages()
     os.chdir(oldpath)
 
