1b692ce0085db69651a687c98215581ce805f979
Andreas Loupasakis <alup@indifex.com>
2010-09-14 18:52:20 +0300

    Cleanup old tests for locks.
     - TODO: we need to put new tests for this app.

--- transifex/addons/locks/tests/test_locking.py
+++ transifex/addons/locks/tests/test_locking.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
-import warnings
-from time import sleep
-from django.core import management
 from django.conf import settings
-from django.db.models.loading import get_model, get_app
 from django.core.urlresolvers import reverse
+from locks.models import Lock
+from resources.models import Resource
+from teams.models import Team
+from languages.models import Language
 from txcommon.tests.base import BaseTestCase
 from txcommon.log import logger
 from notification.models import Notice
-
-# Load models
-POFile = get_model('translations', 'POFile')
-Team = get_model('teams', 'Team')
-Language = get_model('languages', 'Language')
-POFileLock = get_model('locks', 'POFileLock')
 
 # These Languages and POFiles should exist:
 TEAM_LANG_CODES = ['en_US', 'pt_BR', 'el']
@@ -33,217 +27,24 @@ class TestLocking(BaseTestCase):
         settings.LOCKS_LIFETIME = 10
         settings.LOCKS_EXPIRE_NOTIF = 10
 
-        # Create teams
-        for code in TEAM_LANG_CODES:
-            logger.debug("Trying to create team for: %s" % code)
-            language, created = Language.objects.get_or_create(code=code)
-            team, created = self.project.team_set.get_or_create(
-                language=language, creator=self.user['maintainer'])
-            team.members.add(self.user['team_member'])
-
-        # Select first POFile
-        self.pofile = self.pofiles.get(language_code = TEAM_LANG_CODES[0])
-
         # Generate URLs
-        url_args = [self.pofile.object.project.slug,
-            self.pofile.object.slug, self.pofile.filename]
-        self.url_lock = reverse('resource_file_lock', args=url_args)
-        self.url_unlock = reverse('resource_file_unlock', args=url_args)
+        url_args = [self.resource.project.slug,
+            self.resource.slug, self.language.code]
+        self.url_lock = reverse('resource_language_lock', args=url_args)
+        self.url_unlock = reverse('resource_language_unlock', args=url_args)
+        self.url_extend = reverse('resource_language_extend', args=url_args)
         self.url_resource = reverse('resource_detail', args=url_args[:2])
-        self.url_submit = reverse('resource_submit_file', args=url_args)
-        self.url_start_lotte = reverse('resource_edit_file', args=url_args)
+        self.url_start_lotte = reverse('translate_resource', args=url_args)
 
         # Sanity checks
-        self.assertEqual( POFileLock.objects.all().count(), 0)
-        self.assertEqual( POFileLock.objects.valid().count(), 0)
+        self.assertEqual( Lock.objects.all().count(), 0)
+        self.assertEqual( Lock.objects.valid().count(), 0)
 
     def test_lotte(self):
-        # Try opening Lotte and check whether file was locked
+        # Try opening Lotte and check whether resource was locked
         resp = self.client['team_member'].post(self.url_start_lotte, follow = True)
         self.assertEqual( resp.status_code, 200 )
-        self.assertEqual( POFileLock.objects.valid().count(), 1)
-        POFileLock.objects.all().delete()
-
-        # TODO: Check file unlocking, couldn't find proper way to do it ATM
-
-    def test_submission(self):
-        def get_file_handle(pof):
-            return open(self.resource.trans.tm.get_file_path(pof.filename))
-
-        # Enable submission        
-        self.resource.allows_submission = True
-        self.resource.save()
-
-        # Try to submit file as translator: should succeed
-        resp = self.client['team_member'].post(self.url_submit, {'submitted_file':
-            get_file_handle(self.pofile),'message':'Test'}, follow=True)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue("File submitted successfully" in resp.content)
-
-        # Try to submit file as maintainer: should succeed
-        resp = self.client['maintainer'].post(self.url_submit, {'submitted_file':
-            get_file_handle(self.pofile),'message':'Test'}, follow=True)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue("File submitted successfully" in resp.content)
-
-        # Lock the file
-        lock = POFileLock.objects.create_update(self.pofile, self.user['team_member'])
-        
-        # Get the expiration time of current lock
-        expires = self.pofile.locks.get().expires
-        sleep(2)
-
-        # Try to submit file as translator: should succeed
-        resp = self.client['team_member'].post(self.url_submit, {'submitted_file':
-            get_file_handle(self.pofile),'message':'Test'}, follow=True)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue("File submitted successfully" in resp.content)
-
-        # Make sure expiration time didn't change
-        self.assertEqual(self.pofile.locks.get().expires, expires)
-        sleep(2)
-
-        # Try to submit file AND EXTEND LOCK: should succeed
-        resp = self.client['team_member'].post(self.url_submit, {'submitted_file':
-            get_file_handle(self.pofile),'message':'Test', 'lock_extend':'1'}, follow=True)
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue("File submitted successfully" in resp.content)
-        self.assertTrue(self.pofile.locks.get().expires > expires,
-            "Extend lock checkbox doesn't work")
-
-        # Try to submit file as maintainer: should get 403
-        resp = self.client['maintainer'].post(self.url_submit, {'submitted_file':
-            get_file_handle(self.pofile),'message':'Test'}, follow=True)
-        self.assertEqual(resp.status_code, 403)
-
-        # Delete all locks
-        POFileLock.objects.all().delete()
+        self.assertEqual( Lock.objects.valid().count(), 1)
 
 
-    def test_permissions(self):
-        # format: locker : (could_lock, {unlocker : could_unlock}),
-        LOCK_PERM_MATRIX = {
-            'maintainer' : (
-                True,
-                {
-                    'anonymous':False,
-                    'team_member':False,
-                    'maintainer' : True
-                }),
-            'team_member' : (
-                True,
-                {
-                    'anonymous':False,
-                    'team_member':True,
-                    'maintainer' : True
-                }),
-            'anonymous' : (False, {'anonymous':False} )
-        }
-
-        for locker_nick, (could_lock, unlockers) in LOCK_PERM_MATRIX.iteritems():
-            for unlocker_nick, could_unlock in unlockers.iteritems():
-                resp = self.client[locker_nick].post(self.url_lock, follow = True)
-                if could_lock:
-                    logger.debug("Trying to lock as: %s (Should succeed) ..." % locker_nick)
-                    self.assertEqual( resp.status_code, 200 )
-                    url, code = resp.redirect_chain[0]
-                    self.assertTrue( self.url_resource in url )
-                    self.assertEqual( code, 302 )
-                    self.assertEqual( POFileLock.objects.valid().count(), 1)
-                    lock = POFileLock.objects.valid().get()
-                    self.assertEqual( lock.owner, self.user[locker_nick] )
-
-                    resp = self.client[unlocker_nick].post(self.url_unlock, follow = True)
-                    if could_unlock:
-                        logger.debug("Trying to unlock as: %s (Should succeed) ..." % unlocker_nick)
-                        self.assertEqual( POFileLock.objects.valid().count(), 0)
-                    else:
-                        logger.debug("Trying to unlock as: %s (Should fail)" % unlocker_nick)                        
-                        self.assertEqual( POFileLock.objects.valid().count(), 1)
-                else:
-                    logger.debug("Trying to lock as: %s (Should fail) ..." % locker_nick)
-                    self.assertEqual( POFileLock.objects.valid().count(), 0)
-                POFileLock.objects.all().delete()
-
-    def test_main(self):
-        """
-        Test adding locks
-        """
-        # Check
-        teams = []
-        for pofile in self.pofiles:
-
-            value = POFileLock.can_lock(pofile, self.user['team_member'])
-            if pofile.language and pofile.language.code in TEAM_LANG_CODES:
-                teams.append(pofile.language.code)
-                self.assertTrue( value )
-            else:
-                    self.assertFalse( value )
-            # Project maintainers should be able to access locks
-            self.assertTrue(POFileLock.can_lock(pofile,
-                self.user['maintainer']))
-            # Registered but otherwise not associated users shouldn't be able to
-            #access locks
-            self.assertFalse( POFileLock.can_lock(pofile,
-                self.user['registered']))
- 
-        for team in teams:
-            self.assertTrue( team in TEAM_LANG_CODES )
-
-        # Sanity check
-        self.assertEqual(POFileLock.objects.valid().count(), 0)
-        self.assertEqual(POFileLock.objects.all().count(), 0)
-
-        # Try to lock all files
-        for pofile in self.pofiles:
-            try:
-                POFileLock.objects.create_update(pofile,
-                    self.user['team_member'])
-                logger.debug("Locked: %s" % pofile)
-            except:
-                self.assertFalse( pofile.language and pofile.language.code in
-                    TEAM_LANG_CODES )
-
-        # Check whether lock was created and is valid
-        self.assertEqual(POFileLock.objects.all().count(), len(TEAM_LANG_CODES))
-        self.assertEqual(POFileLock.objects.valid().count(),
-            len(TEAM_LANG_CODES))
-
-        
-        for lock in POFileLock.objects.valid():
-            # Check that the lock that exists is for the team we created
-            self.assertTrue(lock.pofile.language.code in TEAM_LANG_CODES)
-
-            # Check permissions again
-            self.assertFalse(lock.can_unlock(self.user['registered']))
-            self.assertTrue(lock.can_unlock(self.user['team_member']))
-            self.assertTrue(lock.can_unlock(self.user['maintainer']))
-
-        # No need to actually delete locks because can_unlock is the only
-        # constraint for delete_by
-        
-        # Now sleep and check lock validness again
-        logger.debug("Sleeping for %i seconds" % settings.LOCKS_LIFETIME)
-        sleep( settings.LOCKS_LIFETIME + 1)
-
-        # Check whether locks expired
-        self.assertEqual(POFileLock.objects.all().count(), len(TEAM_LANG_CODES))
-        self.assertEqual(POFileLock.objects.valid().count(), 0)
-
-        if settings.ENABLE_NOTICES:
-            # TODO: Check mail.outbox also?
-            logger.debug("Sending cron_hourly signal to send notifications about "
-                "lock expiration")
-            management.call_command('cron', interval='hourly')
-            self.assertEqual( Notice.objects.count(), len(TEAM_LANG_CODES))
-        else:
-            warnings.warn("Please set ENABLE_NOTICES to True to run all tests.",
-                UserWarning)
-
-
-        logger.debug("Sending cron_daily signal to clean up database")
-        management.call_command('cron', interval='daily')
-        self.assertEqual(POFileLock.objects.all().count(), 0)
-
-        self.pofiles = POFile.objects.filter(resource = self.resource)
-        pofile = self.pofiles[0]
+    # TODO: Fill in the gap.
