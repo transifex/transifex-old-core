fbb90f2352821d93f8b3e7e2ee3940553a1c1518
Diego Búrigo Zacarão <diegobz@indifex.com>
2010-10-06 18:01:36 -0300

    Added support for outsourced project in the StatsBase class
     - Now the available_languages property checks whether the project
       has outsourced its access control or not. Useful when query for
       teams.

--- transifex/resources/stats.py
+++ transifex/resources/stats.py
@@ -33,11 +33,16 @@ class StatsBase:
         team_lang_ids = None
 
         if isinstance(self.object, Project):
-            team_lang_ids = self.object.team_set.values_list(
-                'language__id', flat=True)
+            project = self.object
         elif isinstance(self.object, Resource):
-            team_lang_ids = self.object.project.team_set.values_list(
-                'language__id', flat=True)
+            project = self.object.project
+
+        # Check whether project has outsourced its translation access control
+        if project.outsource:
+            project = project.outsource
+
+        team_lang_ids = project.team_set.values_list(
+            'language__id', flat=True)
 
         # Extending list of language_ids as necessary.
         if team_lang_ids:
