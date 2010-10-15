a2bc5f3746268688a64f8eb0c24c239920d03f42
Andreas Loupasakis <alup@indifex.com>
2010-10-15 17:46:58 +0300

    Begin support for RTL.
     - A context_processor provides LANGUAGE_DIRECTION var to differentiate the language types.
     - TODO: Fill in the media/css/rtl.css the overriden property values.

--- transifex/txcommon/context_processors.py
+++ transifex/txcommon/context_processors.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from django.utils import translation
 
 def site_section(request):
     """
@@ -20,3 +21,15 @@ def site_url_prefix_processor(request):
     Inserts context variable SITE_URL_PREFIX for absolute URLs
     """
     return {"SITE_URL_PREFIX" : request.build_absolute_uri("/")[:-1] }
+
+
+def bidi(request):
+    """Adds to the context BiDi related variables
+
+    LANGUAGE_DIRECTION -- Direction of current language ('ltr' or 'rtl')
+    """
+    if translation.get_language_bidi():
+        extra_context = { 'LANGUAGE_DIRECTION':'rtl', }
+    else:
+        extra_context = { 'LANGUAGE_DIRECTION':'ltr', }
+    return extra_context
