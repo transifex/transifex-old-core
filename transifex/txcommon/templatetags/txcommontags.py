aecaef34fee706983e4a8a41b9d483ffc7aa1e62
Dimitris Glezos <glezos@indifex.com>
2010-10-15 19:27:50 +0300

    Make home link lazy translated.

--- transifex/txcommon/templatetags/txcommontags.py
+++ transifex/txcommon/templatetags/txcommontags.py
@@ -4,7 +4,7 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.template import Node, NodeList, TemplateSyntaxError
 from actionlog.models import LogEntry
 from transifex.projects.models import Project
