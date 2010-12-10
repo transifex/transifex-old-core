0b24e0413eb5417fc884537fa3396692833ccf9f
Seraphim Mellos <mellos@indifex.com>
2010-12-10 16:21:55 +0200

    Allow maintainers to push translations even when resource has accept_translations set to False

--- transifex/projects/api.py
+++ transifex/projects/api.py
@@ -282,8 +282,9 @@ class ProjectResourceHandler(BaseHandler):
         # Permissions handling
         team = Team.objects.get_or_none(project, language_code)
         check = ProjectPermission(request.user)
-        if not check.submit_translations(team or project) or not \
-            resource.accept_translations:
+        if not check.submit_translations(team or project) or \
+            (not resource.accept_translations and not
+                check.maintain(project)):
             return rc.FORBIDDEN
 
         if "application/json" in request.content_type:
