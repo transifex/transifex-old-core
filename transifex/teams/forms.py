5dd4649f3f53cf0891caae13f64daa701444c244
Diego Búrigo Zacarão <diegobz@indifex.com>
2010-12-15 17:40:48 -0200

    Improved language handling for team forms
     - Now unavailable languages show as disabled instead
       of being hidden from the select fields.
     - Also now editing a team it's possible to change the
       language to a language related to the previous one,
       such as from 'pt' to 'pt_PT' or vice versa.

--- transifex/teams/forms.py
+++ transifex/teams/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.db.models import Q
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+from transifex.languages.models import Language
 from transifex.teams.models import Team, TeamRequest
+from transifex.txcommon.widgets import SelectWithDisabledOptions
 
 from ajax_select.fields import AutoCompleteField, AutoCompleteSelectMultipleField
 
@@ -26,19 +29,48 @@ class TeamSimpleForm(forms.ModelForm):
         self.fields['project'].initial = project.pk
         self.fields['creator'].widget = forms.HiddenInput()
 
-        # Lets filter the language field based on the teams already created
-        # We don't need to list a language if there is a team for it already
-        # Also, when editing a team detail the language must not be changeable
+        # Lets filter the language field based on the teams already created.
+        # We don't need to enable a language if there is a team for it already.
+        # Also, when editing the team details the language must not be changeable
+        # to other complete different languages. It only accepts changing 
+        # language among languages with the same general code, such as pt, 
+        # pt_BR, pt_PT.
         instance = kwargs.get('instance', None)
         if instance:
-            filtered_langs = self.fields["language"].queryset.filter(
-                pk=instance.language.pk)
+            # Getting general language code. 'pt_BR' turns into 'pt'
+            general_code = instance.language.code.split('_')[0]
+
+            # Create list of languages to be disabled excluding the current 
+            # language and also languages for the same general code that do not
+            # have a team already created for the related project.
+            self.disabled_langs = Language.objects.exclude(
+                Q(code=instance.language.code) |
+                ~Q(teams__project=project), Q(code__startswith=general_code)
+                ).values_list('pk', flat=True)
+
+            # We don't need an empty label
             self.fields["language"].empty_label = None
         else:
-            used_langs = Team.objects.filter(project__pk=project.pk).exclude(
-                language__code=language_code).values('language__pk').query
-            filtered_langs = self.fields["language"].queryset.exclude(pk__in=used_langs)
-        self.fields["language"].queryset = filtered_langs
+            # Create list of languages to be disabled excluding the current
+            # language_code and also the languega of teams already created.
+            self.disabled_langs = Team.objects.filter(project=project).exclude(
+                language__code=language_code).values_list('language__pk', flat=True)
+
+        # Setting custom widget with list of ids that should be disabled
+        self.fields["language"].widget = SelectWithDisabledOptions(
+            choices=[(l.pk, l) for l in Language.objects.all()],
+            disabled_choices=self.disabled_langs)
+
+    def clean_language(self):
+        """Make sure language doesn't get a invalid value."""
+        data = self.cleaned_data['language']
+        if isinstance(data, Language):
+            pk = data.pk
+        else:
+            pk = int(data)
+        if pk in self.disabled_langs:
+            raise forms.ValidationError(_(u'Enter a valid value.'))
+        return data
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -56,7 +88,6 @@ class TeamSimpleForm(forms.ModelForm):
         return cleaned_data
 
 
-
 class TeamRequestSimpleForm(forms.ModelForm):
     class Meta:
         model = TeamRequest
@@ -67,12 +98,24 @@ class TeamRequestSimpleForm(forms.ModelForm):
         self.fields['project'].widget = forms.HiddenInput()
         self.fields['project'].initial = project.pk
         self.fields['user'].widget = forms.HiddenInput()
-        
-        # Lets filter the language field based on the teams already created
-        # We don't need to list a language if there is a team for it already
-        used_langs = Team.objects.filter(project__pk=project.pk).exclude(
-            language__code=language_code).values('language__pk').query
-        filtered_langs = self.fields["language"].queryset.exclude(pk__in=used_langs)
-        self.fields["language"].queryset = filtered_langs
 
+        # Create list of languages to be disabled excluding the current
+        # language_code and also the languega of teams already created.
+        self.disabled_langs = Team.objects.filter(project=project).exclude(
+            language__code=language_code).values_list('language__pk', flat=True)
 
+        # Setting custom widget with list of ids that should be disabled
+        self.fields["language"].widget = SelectWithDisabledOptions(
+            choices=[(l.pk, l) for l in Language.objects.all()],
+            disabled_choices=self.disabled_langs)
+
+    def clean_language(self):
+        """Make sure language doesn't get a invalid value."""
+        data = self.cleaned_data['language']
+        if isinstance(data, Language):
+            pk = data.pk
+        else:
+            pk = int(data)
+        if pk in self.disabled_langs:
+            raise forms.ValidationError(_(u'Enter a valid value.'))
+        return data
--- transifex/txcommon/widgets.py
+++ transifex/txcommon/widgets.py
@@ -1,4 +1,7 @@
+from itertools import chain
 from django import forms
+from django.utils.html import escape, conditional_escape
+from django.utils.encoding import force_unicode
 
 class ReadOnlyWidget(forms.Widget):
     """Display a form field value as plain-text just for reading purposes."""
@@ -9,3 +12,27 @@ class ReadOnlyWidget(forms.Widget):
 
     def render(self, name, value, attrs=None):
         return unicode(self.value)
+
+
+class SelectWithDisabledOptions(forms.Select):
+    """
+    Custom Select widget to allow to display disabled options in a Select box.
+
+    Just pass to ``disabled_choices`` the list of ``choices`` integer ids that
+    should be disabled in the rendering.
+
+    NOTE: This is only for redering purposes. Validation in a field or form
+    level is still required.
+    """
+    def __init__(self, attrs=None, choices=(), disabled_choices=()):
+        super(SelectWithDisabledOptions, self).__init__(attrs)
+        self.choices = list(choices)
+        self.disabled_choices = list(disabled_choices)
+
+    def render_option(self, selected_choices, option_value, option_label):
+        option_value = force_unicode(option_value)
+        selected_html = (option_value in selected_choices) and u' selected="selected"' or ''
+        disabled_html = (int(option_value) in self.disabled_choices) and u' disabled="disabled"' or ''
+        return u'<option value="%s"%s>%s</option>' % (
+            escape(option_value), selected_html + disabled_html,
+            conditional_escape(force_unicode(option_label)))
