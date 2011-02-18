fe2c99dec573f9c5927cc4ca249875974946c43a
Diego Búrigo Zacarão <diegobz@indifex.com>
2011-02-18 14:57:58 -0200

    Fixed watches Ajax URLs to avoid cross-subdomain problems

--- transifex/addons/watches/urls.py
+++ transifex/addons/watches/urls.py
@@ -4,11 +4,11 @@ from views import project_toggle_watch, resource_translation_toggle_watch
 
 urlpatterns = patterns('',
     url(
-        regex = '^projects/p/(?P<project_slug>[-\w]+)/toggle_watch/$',
+        regex = '^ajax/p/(?P<project_slug>[-\w]+)/toggle_watch/$',
         view = project_toggle_watch,
         name = 'project_toggle_watch',),
     url(
-        regex = '^projects/p/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/toggle_watch/l/(?P<language_code>[\-_@\w]+)$',
+        regex = '^ajax/p/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<language_code>[\-_@\w]+)/toggle_watch/$',
         view = resource_translation_toggle_watch,
         name = 'resource_translation_toggle_watch',),
 )
