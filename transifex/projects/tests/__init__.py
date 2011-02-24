3ff43160b57d783ddc97c4cb604aaf5474fac5a0
Diego Búrigo Zacarão <diegobz@indifex.com>
2011-02-24 18:04:09 -0300

    Added more unittest related to view permission access

--- transifex/projects/tests/__init__.py
+++ transifex/projects/tests/__init__.py
@@ -2,5 +2,6 @@ from access_control import *
 from models import *
 from templates import *
 from views import *
+from view_permission_access import *
 from private_projects import *
 from api import *
