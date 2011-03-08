3ed77a095cf2caf8aad1c045258880e6410fd5c9
Seraphim Mellos <mellos@indifex.com>
2011-03-08 10:32:37 +0200

    Fixed migration order for languages using south's depends_on statement.

--- transifex/languages/migrations/0003_changed_rules_equations.py
+++ transifex/languages/migrations/0003_changed_rules_equations.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(DataMigration):
 
+    depends_on = (
+        ('resources', '0009_added_translation_wordcount',),
+    )
+
     def forwards(self, orm):
         """
         Drop extra remaining translations after decreasing the nplural of 
