e25b1f4d341209f08834a8e69f16f01a347f9ca6
Apostolos Bessas <mpessas@indifex.com>
2011-05-19 09:18:05 +0300

    Make admin for gtranslate more functional.

--- transifex/addons/gtranslate/admin.py
+++ transifex/addons/gtranslate/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from transifex.addons.gtranslate.models import Gtranslate
 
 class GtranslateAdmin(admin.ModelAdmin):
-    exclude = ('project', )
+    search_fields = ['project', 'project__name']
+    raw_id_fields = ('project', )
+
 
 admin.site.register(Gtranslate, GtranslateAdmin)
