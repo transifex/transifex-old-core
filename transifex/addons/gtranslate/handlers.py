423d3a4c0d303864dd44d748871d4fe945b922c7
Apostolos Bessas <mpessas@indifex.com>
2011-05-19 09:47:44 +0300

    Don't add automatically projects to gtranslate.
    Use the admin to set for specific projects only.

--- transifex/addons/gtranslate/handlers.py
+++ transifex/addons/gtranslate/handlers.py
@@ -5,21 +5,6 @@ from django.conf import settings
 from transifex.projects.signals import project_created, project_deleted
 
 
-def set_gtranslate_use(sender, **kwargs):
-    """
-    Set the use of google translate on project creation.
-    """
-    GtModel = get_model('gtranslate', 'Gtranslate')
-    gt = GtModel(project=sender)
-    for p in GtModel.objects.filter(use_gtranslate=False):
-        if sender.slug == p or (sender.outsource is not None and sender.outsource == p):
-            gt.use_gtranslate = False
-            break
-    else:
-        gt.use_gtranslate = True
-    gt.save()
-
-
 def delete_gtranslate(sender, **kwargs):
     """
     Delete a Gtranslate object after its corresponding projet has been deleted.
@@ -33,6 +18,5 @@ def delete_gtranslate(sender, **kwargs):
 
 
 def connect():
-    project_created.connect(set_gtranslate_use)
     project_deleted.connect(delete_gtranslate)
 
