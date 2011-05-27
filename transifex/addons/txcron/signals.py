00251a03b358c093eafcb5c20eac907d2bbbee89
Diego Búrigo Zacarão <diegobz@indifex.com>
2011-05-27 17:49:28 -0300

    Increased number of txcron signal available

--- transifex/addons/txcron/signals.py
+++ transifex/addons/txcron/signals.py
@@ -12,3 +12,17 @@ cron_twicedaily = Signal()
 
 # xx:00:00
 cron_hourly = Signal()
+
+cron_30min = Signal()
+
+cron_15min = Signal()
+
+cron_10min = Signal()
+
+cron_5min = Signal()
+
+cron_3min = Signal()
+
+cron_1min = Signal()
+
+cron_30sec = Signal()
