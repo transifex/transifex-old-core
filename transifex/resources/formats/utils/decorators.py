3bbf0e4e59f29ead973ccbc1419266cf733f9be8
Apostolos Bessas <mpessas@indifex.com>
2011-06-03 13:41:21 +0300

    Add need_content decorator and make all decorators raise FormatsErrors instead of Exceptions.

--- transifex/resources/formats/utils/decorators.py
+++ transifex/resources/formats/utils/decorators.py
@@ -1,28 +1,37 @@
+from transifex.resources.formats import FormatsError
+
 def need_resource(fn):
     def resource_fn(self, *args, **kw):
         if not self.resource:
-            raise Exception("Resource not specified.")
+            raise FormatsError("Resource not specified.")
         return fn(self, *args, **kw)
     return resource_fn
+
+def need_content(fn):
+    def content_fn(self, *args, **kw):
+        if self.content is None:
+            raise FormatsError("No content given.")
+        return fn(self, *args, **kw)
+    return content_fn
 
 def need_file(fn):
     def file_fn(self, *args, **kw):
         if not self.filename:
-            raise Exception("File not specified.")
+            raise FormatsError("File not specified.")
         return fn(self, *args, **kw)
     return file_fn
 
 def need_language(fn):
     def language_fn(self, *args, **kw):
         if not self.language:
-            raise Exception("Language not specified")
+            raise FormatsError("Language not specified")
         return fn(self, *args, **kw)
     return language_fn
 
 def need_stringset(fn):
     def stringset_fn(self, *args, **kw):
         if not self.stringset:
-            raise Exception("No strings found. Either bind a resource or a file"
+            raise FormatsError("No strings found. Either bind a resource or a file"
                 " to load strings into the handler.")
         return fn(self, *args, **kw)
     return stringset_fn
@@ -30,7 +39,7 @@ def need_stringset(fn):
 def need_compiled(fn):
     def compiled_fn(self, *args, **kw):
         if not self.compiled_template:
-            raise Exception("No template found. Use compile to generate the"
+            raise FormatsError("No template found. Use compile to generate the"
                  " template first")
         return fn(self, *args, **kw)
     return compiled_fn
