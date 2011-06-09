0a47a8c52d52aa30e3b18ef3ca037708dd8b5cdb
Diego Búrigo Zacarão <diegobz@indifex.com>
2011-06-09 14:31:45 -0300

    Display created info on Django admin panel for Storage model

--- transifex/storage/admin.py
+++ transifex/storage/admin.py
@@ -4,6 +4,6 @@ from transifex.storage.models import StorageFile
 class StorageAdmin(admin.ModelAdmin):
     search_fields = ['name', 'size', 'language__name', 'user__username',
         'uuid']
-    list_display = ['name', 'size', 'language', 'user', 'uuid']
+    list_display = ['name', 'size', 'language', 'user', 'uuid', 'created']
 
 admin.site.register(StorageFile, StorageAdmin)
