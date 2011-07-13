75cd4abb00255ffeb7a32263fb99c1db235e1e1b
Apostolos Bessas <mpessas@indifex.com>
2011-07-13 18:33:52 +0300

    minor: fix test case

--- transifex/resources/tests/backends.py
+++ transifex/resources/tests/backends.py
@@ -39,7 +39,7 @@ class TestResourceBackend(TestBackend):
     def test_create(self):
         rb = ResourceBackend()
         res = rb.create(
-            self.project, slug='test', name='Test', method=self.method,
+            self.project, slug='test1', name='Test', method=self.method,
             source_language=self.source_lang, content=self.content,
             user=self.maintainer, extra_data={'accept_translations': True}
         )
