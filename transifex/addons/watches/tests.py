eb705bf01628dacd5bde3f18d4c787d620e782f5
Ratnadeep Debnath <rtnpro@gmail.com>
2011-07-13 15:03:14 +0530

    tests: Fixed tests for addons/watches: tests failed in settings.ENABLE_NOTICES=False

--- transifex/addons/watches/tests.py
+++ transifex/addons/watches/tests.py
@@ -3,10 +3,11 @@ from django.core.urlresolvers import reverse
 from django.test.client import Client
 from transifex.projects.models import Project
 from transifex.resources.models import Resource
-from transifex.txcommon.tests.base import BaseTestCase, USER_ROLES
+from transifex.txcommon.tests.base import BaseTestCase, USER_ROLES, NoticeTypes
 from django.utils import simplejson
+from django.conf import settings
 
-class TestWatches(BaseTestCase):
+class TestWatches(NoticeTypes, BaseTestCase):
 
     def setUp(self):
         super(TestWatches, self).setUp()
@@ -27,9 +28,16 @@ class TestWatches(BaseTestCase):
             resp = self.client[user].get(self.urls['project'])
             resp1 = self.client[user].get(self.urls['resource_actions'])
             if user != 'anonymous':
-                self.assertContains(resp, "watch_toggle(this, '/ajax/p/%s/toggle_watch/')"%self.project.slug, status_code=200)
-                self.assertContains(resp1, "watch_toggle(this, '/ajax/p/%s/resource/%s/l/%s/toggle_watch/')"%
-                                    (self.project.slug, self.resource.slug, self.language.code), status_code=200)
+                if settings.ENABLE_NOTICES:
+                    self.assertContains(resp, "watch_toggle(this, '/ajax/p/%s/toggle_watch/')"%self.project.slug, status_code=200)
+                    self.assertContains(resp1, "watch_toggle(this, '/ajax/p/%s/resource/%s/l/%s/toggle_watch/')"%
+                                        (self.project.slug, self.resource.slug, self.language.code), status_code=200)
+                else:
+                    self.assertNotContains(resp, "watch_toggle(this, '/ajax/p/%s/toggle_watch/')"%self.project.slug, status_code=200)
+                    self.assertNotContains(resp1, "watch_toggle(this, '/ajax/p/%s/resource/%s/l/%s/toggle_watch/')"%
+                                        (self.project.slug, self.resource.slug, self.language.code), status_code=200)
+
+
             else:
                 self.assertNotContains(resp, '''onclick="watch_toggle(this, '/ajax/p/%s/toggle_watch/')" title="Watch it"'''%self.project.slug, status_code=200)
                 self.assertNotContains(resp1, "watch_toggle(this, '/ajax/p/%s/resource/%s/l/%s/toggle_watch/')"%
@@ -44,11 +52,14 @@ class TestWatches(BaseTestCase):
             if user != 'anonymous':
                 self.assertEqual(resp.status_code, 200)
                 json = simplejson.loads(resp.content)        
-                self.assertTrue(json['project'])
-                self.assertEqual(json['url'], '/ajax/p/%s/toggle_watch/'%self.project.slug)
-                self.assertEqual(json['style'], 'watch_remove')
-                self.assertEqual(json['title'], 'Stop watching this project')
-                self.assertEqual(json['error'], None)
+                if settings.ENABLE_NOTICES:
+                    self.assertTrue(json['project'])
+                    self.assertEqual(json['url'], '/ajax/p/%s/toggle_watch/'%self.project.slug)
+                    self.assertEqual(json['style'], 'watch_remove')
+                    self.assertEqual(json['title'], 'Stop watching this project')
+                    self.assertEqual(json['error'], None)
+                else:
+                    self.assertEqual(json['error'], "Notification is not enabled")
             else:
                 self.assertEqual(resp.status_code, 302)
                 
@@ -56,11 +67,14 @@ class TestWatches(BaseTestCase):
             if user != 'anonymous':
                 self.assertEqual(resp.status_code, 200)
                 json = simplejson.loads(resp.content)
-                self.assertTrue(json['project'])
-                self.assertEqual(json['url'], '/ajax/p/%s/toggle_watch/'%self.project.slug)
-                self.assertEqual(json['style'], 'watch_add')
-                self.assertEqual(json['title'], 'Watch this project')
-                self.assertEqual(json['error'], None)
+                if settings.ENABLE_NOTICES:
+                    self.assertTrue(json['project'])
+                    self.assertEqual(json['url'], '/ajax/p/%s/toggle_watch/'%self.project.slug)
+                    self.assertEqual(json['style'], 'watch_add')
+                    self.assertEqual(json['title'], 'Watch this project')
+                    self.assertEqual(json['error'], None)
+                else:
+                    self.assertEqual(json['error'], "Notification is not enabled")
             else:
                 self.assertEqual(resp.status_code, 302)
     
@@ -73,29 +87,42 @@ class TestWatches(BaseTestCase):
             if user not in ['anonymous', 'registered']:
                 self.assertEqual(resp.status_code, 200)
                 json = simplejson.loads(resp.content)
-                self.assertEqual(json['url'], '/ajax/p/%s/resource/%s/l/%s/toggle_watch/'%
-                                 (self.project.slug, self.resource.slug, self.language.code))
-                self.assertEqual(json['style'], 'watch_remove')
-                self.assertEqual(json['title'], 'Stop watching')
-                self.assertEqual(json['error'], None)
+                if settings.ENABLE_NOTICES:
+                    self.assertEqual(json['url'], '/ajax/p/%s/resource/%s/l/%s/toggle_watch/'%
+                                     (self.project.slug, self.resource.slug, self.language.code))
+                    self.assertEqual(json['style'], 'watch_remove')
+                    self.assertEqual(json['title'], 'Stop watching')
+                    self.assertEqual(json['error'], None)
+                else:
+                    self.assertEqual(json['error'], "Notification is not enabled")
             else:
                 if user == 'anonymous':
                     self.assertEqual(resp.status_code, 302)
                 else:
-                    self.assertEqual(resp.status_code, 403)
-            
+                    if settings.ENABLE_NOTICES:
+                        self.assertEqual(resp.status_code, 403)
+                    else:
+                        self.assertContains(resp, "Notification is not enabled", status_code=200)
+
             resp = self.client[user].post(self.url_resource_translation_toggle_watch, {},)
             if user not in ['anonymous', 'registered']:
                 self.assertEqual(resp.status_code, 200)
                 json = simplejson.loads(resp.content)
-                self.assertEqual(json['url'], '/ajax/p/%s/resource/%s/l/%s/toggle_watch/'%
+                if settings.ENABLE_NOTICES:
+                    self.assertEqual(json['url'], '/ajax/p/%s/resource/%s/l/%s/toggle_watch/'%
                                  (self.project.slug, self.resource.slug, self.language.code))
-                self.assertEqual(json['style'], 'watch_add')
-                self.assertEqual(json['title'], 'Watch it')
-                self.assertEqual(json['error'], None)
+                    self.assertEqual(json['style'], 'watch_add')
+                    self.assertEqual(json['title'], 'Watch it')
+                    self.assertEqual(json['error'], None)
+                else:
+                    self.assertEqual(json['error'], "Notification is not enabled")
             else:
                 if user == 'anonymous':
                     self.assertEqual(resp.status_code, 302)
                 else:
-                    self.assertEqual(resp.status_code, 403)
-                
+                    if settings.ENABLE_NOTICES:
+                        self.assertEqual(resp.status_code, 403)
+                    else:
+                        self.assertContains(resp, "Notification is not enabled", status_code=200)
+
+
