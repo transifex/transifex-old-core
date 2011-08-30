1d498b63c0a5e8e6a1890a40d13f81c7fe4c20fb
Apostolos Bessas <mpessas@indifex.com>
2011-08-30 16:15:40 +0300

    Move locks urls under the /ajax/ namespace.
    The urls to locks are ajax calls, so we move them under the /ajax/ namespace.
    This is necessary for locks to work in hubs. See also ticket 774.

--- transifex/addons/locks/urls.py
+++ transifex/addons/locks/urls.py
@@ -5,16 +5,16 @@ from views import resource_language_lock, resource_language_unlock
 
 urlpatterns = patterns('',
     url(
-        regex = r'^projects/p/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<language_code>[\-_@\w\.]+)/lock/$',
+        regex = r'^ajax/projects/p/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<language_code>[\-_@\w\.]+)/lock/$',
         view = resource_language_lock,
         name = 'resource_language_lock',),
     url(
-        regex = r'^projects/p/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<language_code>[\-_@\w\.]+)/unlock/$',
+        regex = r'^ajax/projects/p/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<language_code>[\-_@\w\.]+)/unlock/$',
         view = resource_language_unlock,
         name = 'resource_language_unlock',),
     # We exploit the create_update to do the extend lock action with the same view as lock creation.
     url(
-        regex = r'^projects/p/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<language_code>[\-_@\w\.]+)/extend/$',
+        regex = r'^ajax/projects/p/(?P<project_slug>[-\w]+)/resource/(?P<resource_slug>[-\w]+)/l/(?P<language_code>[\-_@\w\.]+)/extend/$',
         view = resource_language_lock,
         name = 'resource_language_extend',),
 )
