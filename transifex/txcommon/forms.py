5b8910ea1be98e17c9a46ce34fc0f310c2561008
John Kalantzis <jkal@indifex.com>
2011-11-04 16:46:14 +0200

    Modifications to the edit profile form.

--- transifex/txcommon/forms.py
+++ transifex/txcommon/forms.py
@@ -1,10 +1,14 @@
+from django import forms
 from userena.forms import EditProfileForm as UserenaEditProfileForm
 from userena.utils import get_profile_model
 
 class EditProfileForm(UserenaEditProfileForm):
 
+    def __init__(self, *args, **kw):
+        super(forms.ModelForm, self).__init__(*args, **kw)
+
     class Meta:
         model = get_profile_model()
         exclude = ('user', 'privacy',)
-
-
+        fields = ('first_name', 'last_name', 'mugshot', 'language', 'blog',
+            'linked_in', 'twitter', 'about', 'looking_for_work', 'location')
