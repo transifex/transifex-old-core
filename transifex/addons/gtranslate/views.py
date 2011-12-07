89cad3e0b991fea71ca929f2b19b6a8328f3e298
John Kalantzis <jkal@indifex.com>
2011-12-07 19:04:40 +0200

    autotranslate: Fix for language codes with underscores.

--- transifex/addons/gtranslate/views.py
+++ transifex/addons/gtranslate/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import get_object_or_404
 from gtranslate.models import Gtranslate
 import requests
 
+def _get_canonical_name(target_lang):
+    if '_' in target_lang or '-' in target_lang:
+        return target_lang[:2]
+    return target_lang
+
+
 def autotranslate_proxy(request, project_slug):
     source_lang = request.GET.get('source')
     target_lang = request.GET.get('target')
@@ -12,6 +18,7 @@ def autotranslate_proxy(request, project_slug):
     if not all([source_lang, target_lang, term]):
         return HttpResponse(status=400)
 
+    target_lang = _get_canonical_name(target_lang)
     service = get_object_or_404(Gtranslate, project__slug=project_slug)
     if service.service_type == 'GT':
         base_url = 'https://www.googleapis.com/language/translate/v2'
@@ -39,6 +46,7 @@ def supported_langs(request, project_slug):
     if service.service_type == 'GT':
         base_url = 'https://www.googleapis.com/language/translate/v2/languages'
         target_lang = request.GET.get('target')
+        target_lang = _get_canonical_name(target_lang)
         if not target_lang:
             return HttpResponse(status=400)
         params = {
