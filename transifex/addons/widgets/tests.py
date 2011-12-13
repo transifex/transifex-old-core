0642f26a6a969281eb0506045d1294427e22e188
John Kalantzis <jkal@indifex.com>
2011-12-13 13:29:08 +0200

    widgets: Remove unnecessary broken test case.

--- transifex/addons/widgets/tests.py
+++ transifex/addons/widgets/tests.py
@@ -17,10 +17,5 @@ class TestCharts(BaseTestCase):
         resp = self.client['registered'].get(reverse('project_widgets',
             kwargs={'project_slug': self.project.slug}))
         self.assertEqual(resp.status_code, 200)
-
-        # Check if django-staticfiles is serving widgets CSS
-        resp = self.client['registered'].get("%swidgets/css/widgets.css" % settings.STATIC_URL, follow = True)
-        self.assertEqual(resp.status_code, 200, msg = "Please run ./manage.py build_static")
-
         # Check if we got correct file
         self.assertTrue("code_snippet" in resp.content)
