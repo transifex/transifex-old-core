d0ec7a5a37ef89c28cbe6cb00ef6f19dec98d7da
Apostolos Bessas <mpessas@indifex.com>
2011-12-19 15:37:15 +0200

    Refactor properties formats to use the same super class.

--- transifex/resources/formats/javaproperties.py
+++ transifex/resources/formats/javaproperties.py
@@ -10,24 +10,29 @@ from transifex.txcommon.log import logger
 from transifex.resources.models import SourceEntity
 from transifex.resources.formats.utils.decorators import *
 from transifex.resources.formats.utils.hash_tag import hash_tag
-from transifex.resources.formats.core import Handler, ParseError, CompileError
+from transifex.resources.formats.properties import PropertiesHandler, \
+        PropertiesParseError, PropertiesCompileError
 from transifex.resources.formats.resource_collections import StringSet, \
         GenericTranslation
 
 
-class JavaParseError(ParseError):
+class JavaParseError(PropertiesParseError):
     pass
 
 
-class JavaCompileError(CompileError):
+class JavaCompileError(PropertiesCompileError):
     pass
 
 
-class JavaPropertiesHandler(Handler):
-    """
-    Handler for Java PROPERTIES translation files.
+class JavaPropertiesHandler(PropertiesHandler):
+    """Handler for Java PROPERTIES translation files.
 
     Java properties files *must* be encoded in cls.default_encoding encoding.
+
+    See
+    http://download.oracle.com/javase/1.4.2/docs/api/java/util/PropertyResourceBundle.html,
+    http://download.oracle.com/javase/1.4.2/docs/api/java/util/Properties.html#encoding and
+    http://download.oracle.com/javase/1.4.2/docs/api/java/util/Properties.html#load(java.io.InputStream)
     """
 
     name = "Java *.PROPERTIES file handler"
@@ -37,63 +42,6 @@ class JavaPropertiesHandler(Handler):
 
     HandlerParseError = JavaParseError
     HandlerCompileError = JavaCompileError
-
-    SEPARATORS = [' ', '\t', '\f', '=', ':', ]
-    comment_chars = ('#', '!', )
-
-    def _escape(self, s):
-        """
-        Escape special characters in Java properties files.
-
-        Java escapes the '=' and ':' in the value
-        string with backslashes in the store method.
-        So let us do the same.
-        """
-        return (s.replace(':', '\:')
-                .replace('=', '\=')
-                .replace('\\', '\\\\')
-        )
-
-    def _is_escaped(self, line, index):
-        """
-        Returns True, if the character at index is escaped by backslashes.
-
-        There has to be an even number of backslashes before the character for
-        it to be escaped.
-        """
-        nbackslashes = 0
-        for c in reversed(line[:index]):
-            if c == '\\':
-                nbackslashes += 1
-            else:
-                break
-        return nbackslashes % 2 == 1
-
-    def _split(self, line):
-        """
-        Split a line in (key, value).
-
-        The separator is the first non-escaped charcter of (\s,=,:).
-        If no such character exists, the wholi line is a key with no value.
-        """
-        for i, c in enumerate(line):
-            if c in self.SEPARATORS and not self._is_escaped(line, i):
-                # Seperator found
-                key = line[:i].lstrip()
-                value = self._strip_separators(line[i+1:])
-                return (key, value)
-        return (line, None)
-
-    def _strip_separators(self, s):
-        """Strip separators from the front of the string s."""
-        return s.lstrip(''.join(self.SEPARATORS))
-
-    def _unescape(self, value):
-        """Reverse the escape of special characters."""
-        return (value.replace('\:', ':')
-                     .replace('\=', '=')
-                     .replace('\\\\', '\\')
-        )
 
     def convert_to_unicode(self, s):
         """Convert the string s to a proper unicode string.
@@ -150,87 +98,12 @@ class JavaPropertiesHandler(Handler):
         return re.sub(re.escape(original), replacement, text)
 
 
-    def _parse(self, is_source, lang_rules):
-        """
-        Parse a java .properties content and create a stringset with
-        all entries in it.
-
-        See
-        http://download.oracle.com/javase/1.4.2/docs/api/java/util/PropertyResourceBundle.html,
-        http://download.oracle.com/javase/1.4.2/docs/api/java/util/Properties.html#encoding and
-        http://download.oracle.com/javase/1.4.2/docs/api/java/util/Properties.html#load(java.io.InputStream)
-        for details.
-        """
-        resource = self.resource
-
-        context = ""
-        self._find_linesep(self.content)
-        template = u""
-        key_dict = {}
-        rule = 5
-        lines = self._iter_by_line(self.content)
-        for line in lines:
-            line = self._prepare_line(line)
-            # Skip empty lines and comments
-            if not line or line.startswith(self.comment_chars):
-                if is_source:
-                    template += line + self.linesep
-                continue
-            # If the last character is a backslash
-            # it has to be preceded by a space in which
-            # case the next line is read as part of the
-            # same property
-            while line[-1] == '\\' and not self._is_escaped(line, -1):
-                # Read next line
-                nextline = self._prepare_line(lines.next())
-                # This line will become part of the value
-                line = line[:-1] + self._prepare_line(nextline)
-            key, value = self._split(line)
-
-            if value is not None:
-                uni_chars = re.findall(r'(\\u[0-9A-Fa-f]{4})', value)
-                for uni_char in uni_chars:
-                    value = value.replace(
-                        uni_char, self.convert_to_unicode(uni_char)
-                    )
-
-            if is_source:
-                if not value:
-                    template += line + self.linesep
-                    # Keys with no values should not be shown to translator
-                    continue
-                else:
-                    key_len = len(key)
-                    template += line[:key_len] + re.sub(
-                        re.escape(value),
-                        "%(hash)s_tr" % {'hash': hash_tag(key, context)},
-                        line[key_len:]
-                    ) + self.linesep
-            elif not SourceEntity.objects.filter(resource=resource, string=key).exists():
-                # ignore keys with no translation
-                continue
-
-            if key in key_dict and key_dict[key].get(rule, None):
-                g = GenericTranslation(key, self._unescape(
-                    key_dict[key][rule]['translation']),
-                    context=key_dict[key][rule]['context'])
-                self.stringset.strings.remove(g)
-            else:
-                if key in key_dict:
-                    key_dict[key][rule] = {
-                                            'translation':self._unescape(value),
-                                            'context': context
-                                          }
-                else:
-                    key_dict[key] = {
-                                rule: {
-                                    'translation': self._unescape(value),
-                                    'context': context
-                                }
-                            }
-
-            self._add_translation_string(
-                key, self._unescape(value), context=context
-            )
-        return template
-
+    def _visit_value(self, value):
+        """Convert the value to unicode-escaped string."""
+        if value is not None:
+            uni_chars = re.findall(r'(\\u[0-9A-Fa-f]{4})', value)
+            for uni_char in uni_chars:
+                value = value.replace(
+                    uni_char, self.convert_to_unicode(uni_char)
+                )
+        return value
