ddaf64b2498381955834c9b785a2c7d1f0ff4f9d
John Kalantzis <jkal@indifex.com>
2011-12-23 12:11:01 +0200

    Allow changing the project language from the form when there are no resources.

--- transifex/projects/forms.py
+++ transifex/projects/forms.py
@@ -40,8 +40,9 @@ class ProjectForm(forms.ModelForm):
         super(ProjectForm, self).__init__(*args, **kwargs)
         # Disable the source_language widget when updating
         if self.instance and self.instance.id:
-            self.fields['source_language'].required = False
-            self.fields['source_language'].widget.attrs['disabled'] = 'disabled'
+            if self.instance.resources.count():
+                self.fields['source_language'].required = False
+                self.fields['source_language'].widget.attrs['disabled'] = 'disabled'
         project_form_init.send(sender=ProjectForm, form=self)
 
     def save(self, *args, **kwargs):
@@ -61,7 +62,10 @@ class ProjectForm(forms.ModelForm):
 
     def clean_source_language(self):
         if self.instance and self.instance.id:
-            return self.instance.source_language
+            if self.instance.resources.count():
+                return self.instance.source_language
+            else:
+                return self.cleaned_data['source_language']
         else:
             return self.cleaned_data['source_language']
 
