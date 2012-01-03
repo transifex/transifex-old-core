a084b29d6fec7489ebf5c4f00b53b967d04aa9cb
Diego Búrigo Zacarão <diegobz@indifex.com>
2012-01-03 14:48:01 -0200

    Made urlconf a bit more flexible
     - A set of urls now can be easily disabled just by changing the
       settings.ROOT_URLCONF

--- transifex/projects/urls/__init__.py
+++ transifex/projects/urls/__init__.py
@@ -56,10 +56,6 @@ urlpatterns = patterns('',
 # Project
 urlpatterns += patterns('',
     url(
-        regex = '^myprojects/$',
-        view = myprojects,
-        name = 'myprojects'),
-    url(
         regex = '^add/$',
         view = project_create,
         name = 'project_create'),
@@ -122,28 +118,6 @@ urlpatterns += patterns('',
 
 
 urlpatterns += patterns('django.views.generic',
-    url(
-        regex = '^$',
-        view = 'list_detail.object_list',
-        kwargs = public_project_list,
-        name = 'project_list'),
-    url(
-        '^recent/$', 'list_detail.object_list',
-        kwargs = {
-            'queryset': Project.public.recent(),
-            'template_object_name': 'project',
-            'extra_context' : {'type_of_qset' : 'projects.recent',},
-        },
-        name = 'project_list_recent'),
-    url (
-        regex = '^open_translations/$',
-        view = 'list_detail.object_list',
-        kwargs = {
-            'queryset': Project.public.open_translations(),
-            'template_object_name': 'project',
-            'extra_context' : {'type_of_qset' : 'projects.open_translations',},
-        },
-        name = 'project_list_open_translations'),
     url(
         r'^tag/(?P<tag>[^/]+)/$',
         tagged_object_list,
