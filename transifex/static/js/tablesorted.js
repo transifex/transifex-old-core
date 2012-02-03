0ee855621d4eb5dd2ad7fac6d68c794c7c40e2a5
Antonios E. Garnelis <antoine@indifex.com>
2012-02-03 23:37:32 +0100

    Reorganize some of the tables js code. Fix markup issues and some more

--- transifex/static/js/tablesorted.js
+++ transifex/static/js/tablesorted.js
@@ -15,81 +15,88 @@ function toggle_entries(entries_status){
 
 $(document).ready(function(){
 
-    // ordering for Release stats table
-    $("#stats_release").tablesorter({
-        widgets: ['zebra'],
-        headers: {
-            1: { sorter: "percent"}
-        },
-        textExtraction: { // Take value inside an object for the columns
-            0: function(node) {
-                return $("a", node).text();
-            },
-            1: function(node) {
-                return $(".stats_string_comp", node).text();
-            }
-        }
-    });
+  $(".tablesorter.teamlist").tablesorter({
+    sortList: [[0,0]],
+    textExtraction: function(node) {
+      switch (node.cellIndex){
+          // Stats column
+          case 1:
+              return $(".stats_string_resource", node).text();
+          // Last update column
+          case 2:
+              return $("span", node).attr('unixdate');
+          default: return node.innerHTML;
+      }
+    }        
+  });
+  
+  $(".tablesorter.resdetail").tablesorter({
+    sortList: [[1,0]], 
+    headers: {
+      1: { sorter: 'percent' },
+      2: { sorter: 'number' },
 
-    // ordering for Language x Release stats table
-    $("#stats_lang").tablesorter({
-        widgets: ['zebra'],
-        headers: {
-            1: { sorter: "percent"},
-            2: { sorter: false } // Do not sort the third column
-        },
-        textExtraction: { // Take value inside an object for the columns
-            0: function(node) {
-                return $("a", node).text();
-            },
-            1: function(node) {
-                return $(".stats_string_comp", node).text();
-            }
-        }
-    });
+    },
+    textExtraction: function(node) {  
+      switch (node.cellIndex){
+        // Stats column
+        case 1:
+          return $(".stats_string_resource", node).text();
+        // Last update column
+        case 2:
+          return $("span", node).attr('unixdate');
+        // Priority column
+        default: return node.innerHTML;
+      }
+    } 
+  });
 
-    // FIXME: If this is not used, remove it.
-    // ordering for Component stats table
-        // Just enable sorting for the stats column if there are stats
-        if (typeof calcstats == "undefined" || calcstats == true){
-            statscol = "percent";
-        }else{
-            statscol = false;
-        }
+  $(".tablesorter.reldetail").tablesorter({
+      sortList: [[1,1]],
+      widgets: ['zebra'],
+      headers: {
+          1: { sorter: 'percent' },
+          2: { sorter: 'number' }
+      },
+      textExtraction: function(node) {
+          switch (node.cellIndex){
+              // Stats column
+              case 1:
+                  return $(".stats_string_resource", node).text();
+              // Last update column
+              case 2:
+                  return $("span", node).attr('unixdate');
+              default: return node.innerHTML;
+          }
+      }
+  });
+    
+    
+  $(".tablesorter.langdetail").tablesorter({
+    sortList: [[1,0]], 
+    headers: {
+      1: { sorter: 'percent' },
+      2: { sorter: 'number' },
+      3: { sorter: 'number' }
+    },
+    textExtraction: function(node) {  
+      switch (node.cellIndex){
+        // Stats column
+        case 1:
+            return $(".stats_string_resource", node).text();
+        // Last update column
+        case 2:
+            return $("span", node).attr('unixdate');
+        // Priority column
+        case 3:
+            return $(".priority_sort", node).text()
+        default: return node.innerHTML;
+      }
+    } 
+  });
 
-        $("#stats_comp").tablesorter({
-            widgets: ['zebra'],
-            headers: {
-                1: { sorter: statscol },
-                2: { sorter: false } // Do not sort the third column
-            },
-            textExtraction: { // Take value inside an object for the columns
-                0: function(node) {
-                    return $("a", node).text();
-                },
-                1: function(node) {
-                    return $(".stats_string_comp", node).text();
-                }
-            }
-        });
 
-    // FIXME: If this is not used, remove it.
-    // ordering for Component Multifile Language stats table
-    $("#stats_comp_lang").tablesorter({
-        widgets: ['zebra'],
-        headers: {
-            1: { sorter: "percent"},
-            2: { sorter: false } // Do not sort the third column
-        },
-        textExtraction: { // Take value inside an object for the columns
-            0: function(node) {
-                return $("a", node).text();
-            },
-            1: function(node) {
-                return $(".stats_string_comp", node).text();
-            }
-        }
-    });
+
 
 
     // ordering for Web editing form table
