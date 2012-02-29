293cc1789c13ffafc84468a7f55d1a8bd511ed91
Apostolos Bessas <mpessas@indifex.com>
2012-02-29 15:56:47 +0200

    Store recent logs for resources in redis and expose them through a templatetag.

---
+++ transifex/actionlog/queues.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+"""
+Redis related stuff for action logs.
+"""
+
+from django.db.models import get_model
+from django.utils.encoding import force_unicode
+from transifex.txcommon.log import logger
+from datastores.txredis import TxRedisMapper, redis_exception_handler
+
+
+def redis_key_for_resource(resource):
+    return 'resource:history:%s:%s' % (resource.project_id, resource.slug)
+
+
+def redis_key_for_project(project):
+    return 'project:history:%s' % project.slug
+
+
+@redis_exception_handler
+def log_to_queues(o, user_id, action_time, message):
+    """Log actions to redis' queues."""
+    Project = get_model('projects', 'Project')
+    Resource = get_model('resources', 'Resource')
+    if isinstance(o, Project):
+        _log_to_recent_project_actions(o, user_id, action_time, message)
+        _log_to_project_history(o, action_time, message)
+    elif isinstance(o, Resource):
+        _log_to_resource_history(o, action_time, message)
+
+
+def _log_to_recent_project_actions(p, user_id, action_time, message):
+    """Log actions that refer to projects to a queue of most recent actions.
+
+    We use redis' list for that. We skip actions that refer to private projects.
+    """
+    Project = get_model('projects', 'Project')
+    if p.private:
+        return
+    private_slugs = Project.objects.filter(
+        private=True
+    ).values_list('slug', flat=True)
+    for slug in private_slugs:
+        if ('/projects/p/%s/' % slug) in message:
+            return
+
+    key = 'event_feed'
+    data = {
+        'name': force_unicode(p)[:200],
+        'user_id': user_id,
+        'action_time': action_time,
+        'message': message
+    }
+    r = TxRedisMapper()
+    r.lpush(key, data=data)
+    r.ltrim(key, 0, 11)
+
+
+@redis_exception_handler
+def _log_to_project_history(project, action_time, message):
+    """Log a message to a project's history queue."""
+    Project = get_model('projects', 'Project')
+    key = project.recent_history_key
+    data = {
+        'action_time': action_time,
+        'message': message,
+    }
+    r = TxRedisMapper()
+    r.lpush(key, data=data)
+    r.ltrim(key, 0, 4)
+
+
+@redis_exception_handler
+def _log_to_resource_history(resource, action_time, message):
+    """Log a message to a respurce's history queue."""
+    Resource = get_model('resource', 'Resource')
+    key = redis_key_for_resource(resource)
+    data = {
+        'action_time': action_time,
+        'message': message,
+    }
+    r = TxRedisMapper()
+    r.lpush(key, data=data)
+    r.ltrim(key, 0, 4)
--- transifex/actionlog/templatetags/tx_action_log.py
+++ transifex/actionlog/templatetags/tx_action_log.py
@@ -1,5 +1,9 @@
 from django import template
+from django.conf import settings
+from django.db.models import get_model
 from actionlog.models import LogEntry
+from actionlog.queues import redis_key_for_resource
+from datastores.txredis import TxRedisMapper, redis_exception_handler
 
 register = template.Library()
 
@@ -68,5 +72,50 @@ class DoGetLog:
                     "Fourth argument in '%s' must be either 'user' or "
                     "'object'" % self.tag_name)
 
+
+class RecentLogNode(template.Node):
+    """Node to get the most recent action logs for an item."""
+
+    def __init__(self, model, obj, key_func, context_var):
+        self.model = model
+        self.obj = obj
+        self.key_func = key_func
+        self.context_var = context_var
+
+    def render(self, context):
+        self.obj = template.Variable(self.obj).resolve(context)
+        redis_key = self.key_func(self.obj)
+        events = self._action_logs_from_redis(redis_key)
+        if events is None:
+            events = LogEntry.objects.filter(
+                content_type=ContentType.objects.get_for_model(self.model),
+                object_id=self.obj.id
+            )
+        context[self.context_var] = events
+
+    @redis_exception_handler
+    def _action_logs_from_redis(self, key):
+        """Get the action logs for the key from redis."""
+        if not settings.USE_REDIS:
+            return None
+        r = TxRedisMapper()
+        return r.lrange(key, 0, -1)
+
+
+def recent_resource_log(parser, token):
+    """Return the most recent logs of the specified resource."""
+    tokens = token.split_contents()
+    if len(tokens) != 4:
+        msg = "Wrong number of arguments for %s."
+        raise template.TemplateSyntaxError(msg % tokens[0])
+    elif tokens[2] != 'as':
+        msg = "Wrong syntax for %s: third argument must be the keyword 'as'"
+        raise template.TemplateSyntaxError(msg % tokens[0])
+    resource = tokens[1]
+    context_var = tokens[3]
+    Resource = get_model('resources', 'Resource')
+    return RecentLogNode(Resource, resource, redis_key_for_resource, context_var)
+
 register.tag('get_log', DoGetLog('get_log'))
 register.tag('get_public_log', DoGetLog('get_public_log'))
+register.tag('recent_resource_log', recent_resource_log)
