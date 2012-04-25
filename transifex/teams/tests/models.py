c0dbb30946c9b7ddb49d10cc0d1b562faf2e07ec
rtnpro <rtnpro@transifex.com>
2012-04-26 01:55:37 +0530

    Added for_user() method in Team manager to fetch all teams that a user has access to.

--- transifex/teams/tests/models.py
+++ transifex/teams/tests/models.py
@@ -25,4 +25,12 @@ class TestTeamModels(base.BaseTestCase):
         # There must be 2 teams. One 'pt_BR' and a 'ar' one.
         self.assertEquals(project.available_teams.count(), 2)
 
+    def test_teams_for_user(self):
+        for user in ['reviewer', 'team_member', 'team_coordinator',
+                'maintainer']:
+            self.assertEqual(set(Team.objects.for_user(self.user[user]
+                ).values_list('pk')),
+                    set(Team.objects.filter(pk__in=[self.team.pk,
+                        self.team_private.pk]).values_list('pk'))
+            )
 
