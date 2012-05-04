7c1ce9d4a58d0f2c8cbcdb1b1d8201f83a29e807
Antonios E. Garnelis <antoine@transifex.com>
2012-05-04 14:26:24 +0200

    initial commit of persisthide jquery plugin

---
+++ transifex/static/js/persisthide.js
@@ -0,0 +1,28 @@
+(function($){
+  $.fn.extend({ 
+    persisthide: function() {
+      return this.each(function() {
+        
+        phobj = $(this);
+        phid = $(this).attr("id")+"-ph";
+
+        if (amplify.store(phid) == 'expanded') {
+          phobj.addClass("expanded").children(".ph-content").show();
+        }
+
+        phobj.children(".ph-trigger").click(function(){
+          if(phobj.hasClass("expanded")){
+        		   phobj.removeClass("expanded")
+        		        .children(".ph-content").hide();
+            amplify.store(phid, 'contracted');
+          }
+          else{
+               phobj.addClass("expanded")
+                    .children(".ph-content").show();
+            amplify.store(phid, 'expanded');
+          }
+        });
+      });
+    }
+  });
+})(jQuery);
