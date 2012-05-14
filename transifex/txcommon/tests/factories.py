c912668221b7b64842de032c64601b6ee6bad2dc
Apostolos Bessas <mpessas@transifex.com>
2012-05-14 13:26:56 +0300

    Add a factory class for requests for tests.
    It is a class that can mock a request in tests.

---
+++ transifex/txcommon/tests/factories.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Factories to easily create test objects.
+"""
+
+
+class RequestFactory(object):
+    """Factory to create simple request objects."""
+
+    META = {}
