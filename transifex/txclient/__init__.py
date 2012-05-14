8db7bcd77c850d6632024733b4f65d59f8ead46c
Apostolos Bessas <mpessas@transifex.com>
2012-05-14 13:25:07 +0300

    Add the txclient app.
    The specific app tries to react on requests coming from the client.

---
+++ transifex/txclient/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Add support to Transifex for the client.
+
+The app allows Transifex to detect the client and act accordingly.
+"""
+
+
+from __future__ import absolute_import
+from .utils import handle_client_request
